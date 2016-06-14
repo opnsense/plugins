89666126e1bb866820b75b05b1570e827a985e4e
Franco Fichtner <franco@opnsense.org>
2016-06-14 09:30:18 +0200

    net/haproxy: open up template on/off and run setup.sh on boot

--- net/haproxy/src/opnsense/service/templates/OPNsense/HAProxy/rc.conf.d
+++ net/haproxy/src/opnsense/service/templates/OPNsense/HAProxy/rc.conf.d
@@ -1,4 +1,9 @@
-haproxy_enable="{% if helpers.exists('OPNsense.HAProxy.general.enabled') and OPNsense.HAProxy.general.enabled|default("0") == "1" %}YES{% else %}NO{% endif %}"
+{% if helpers.exists('OPNsense.HAProxy.general.enabled') and OPNsense.HAProxy.general.enabled|default("0") == "1" %}
+haproxy_enable=YES
+haproxy_opnsense_bootup_run="/usr/local/opnsense/scripts/OPNsense/HAProxy/setup.sh"
 haproxy_pidfile="/var/run/haproxy.pid"
 haproxy_config="/usr/local/etc/haproxy.conf"
 # haproxy_flags=""
+{% else %}
+haproxy_enable=NO
+{% endif %}
