17de3033bb278a727d16f184e823e9a3c42d3c9c
gitdevmod <gitdevmod@github.com>
2016-06-22 11:39:56 +0200

    Change SSOActiveDirectory to sso_ad Adjust PLUGINS_DEPENDS in Makefile

---
+++ sysutils/sso_ad/src/mvc/app/controllers/OPNsense/sso_ad/Api/ServiceController.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ *    Copyright (C) 2016 gitdevmod@github.com
+ *
+ *    All rights reserved.
+ *
+ *    Redistribution and use in source and binary forms, with or without
+ *    modification, are permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ *    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ *    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ *    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *    POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+namespace OPNsense\sso_ad\Api;
+
+use \OPNsense\Base\ApiControllerBase;
+use \OPNsense\Core\Backend;
+class ServiceController extends ApiControllerBase
+{
+
+public function reloadAction()
+{
+    $status = "failed";
+    if ($this->request->isPost()) {
+        $backend = new Backend();
+        $bckresult = trim($backend->configdRun("template reload OPNsense.sso_ad"));
+        if ($bckresult == "OK") {
+            $status = "ok";
+        }
+    }
+    return array("status" => $status);
+}
+
+public function testAction()
+{
+    if ($this->request->isPost()) {
+        $backend = new Backend();
+        $bckresult = json_decode(trim($backend->configdRun("sso_ad test")), true);
+        if ($bckresult !== null) {
+            // only return valid json type responses
+            return $bckresult;
+        }
+    }
+    return array("message" => "unable to run config action");
+}
+
+}
