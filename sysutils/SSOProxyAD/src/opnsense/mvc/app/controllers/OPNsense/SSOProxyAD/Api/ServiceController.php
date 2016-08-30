c08430182c6edf340269e8eb8dad98549ad0c6de
System Administrator <root@OPNsense.test.lan>
2016-08-31 00:53:57 +0200

    Add missing use

--- sysutils/SSOProxyAD/src/opnsense/mvc/app/controllers/OPNsense/SSOProxyAD/Api/ServiceController.php
+++ sysutils/SSOProxyAD/src/opnsense/mvc/app/controllers/OPNsense/SSOProxyAD/Api/ServiceController.php
@@ -30,6 +30,7 @@
 namespace OPNsense\SSOProxyAD\Api;
 
 use \OPNsense\Base\ApiControllerBase;
+use \OPNsense\SSOProxyAD\SSOProxyAD;
 use \OPNsense\Core\Backend;
 use \OPNsense\Cron\Cron;
 class ServiceController extends ApiControllerBase
