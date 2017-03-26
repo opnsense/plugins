130ce651eeeb785a710c4566530e427a000c714d
Franco Fichtner <franco@opnsense.org>
2017-03-26 14:33:34 +0200

    security/acme-client: merge version 1.3 from master

--- security/acme-client/src/opnsense/scripts/OPNsense/AcmeClient/setup.sh
+++ security/acme-client/src/opnsense/scripts/OPNsense/AcmeClient/setup.sh
@@ -5,7 +5,7 @@ ACME_DIRS="/var/etc/acme-client /var/etc/acme-client/certs /var/etc/acme-client/
 for directory in ${ACME_DIRS}; do
     mkdir -p ${directory}
     chown -R root:wheel ${directory}
-    chmod -R 755 ${directory}
+    chmod -R 750 ${directory}
 done
 
 exit 0
