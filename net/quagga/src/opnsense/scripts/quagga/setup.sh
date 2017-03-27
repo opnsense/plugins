a504fdbef203335163f8a55af9e7f357e22d2dca
Franco Fichtner <franco@opnsense.org>
2017-03-27 22:40:04 +0200

    net/quagga: more changes for version 0.0.2

--- net/quagga/src/opnsense/scripts/quagga/setup.sh
+++ net/quagga/src/opnsense/scripts/quagga/setup.sh
@@ -3,11 +3,10 @@
 user=quagga
 group=quagga
 
-mkdir -p /var/run/quagga 
-chown $user:$group /var/run/quagga 
+mkdir -p /var/run/quagga
+chown $user:$group /var/run/quagga
 chmod 750 /var/run/quagga
 
-mkdir -p /usr/local/etc/quagga 
-chown $user:$group /usr/local/etc/quagga 
+mkdir -p /usr/local/etc/quagga
+chown $user:$group /usr/local/etc/quagga
 chmod 750 /usr/local/etc/quagga
- 
