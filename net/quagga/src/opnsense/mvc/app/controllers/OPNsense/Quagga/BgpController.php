5794953ede72950b8935e315583d68fcc7b753d0
Michael <m.muenz@gmail.com>
2017-04-04 14:54:20 +0200

    Update BgpController.php

--- net/quagga/src/opnsense/mvc/app/controllers/OPNsense/Quagga/BgpController.php
+++ net/quagga/src/opnsense/mvc/app/controllers/OPNsense/Quagga/BgpController.php
@@ -26,7 +26,7 @@ class BgpController extends \OPNsense\Base\IndexController
     public function indexAction()
     {
         $this->view->title = gettext("BGP Settings");
-        $this->view->generalForm = $this->getForm("bgp");
+        $this->view->bgpForm = $this->getForm("bgp");
         $this->view->pick('OPNsense/Quagga/bgp');
     }
 }
