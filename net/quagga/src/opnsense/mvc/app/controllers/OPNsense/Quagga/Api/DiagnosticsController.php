4b753a4ad63f158a08f30954531ee36d61b3f305
Michael <m.muenz@gmail.com>
2017-04-26 14:37:28 +0200

    Update DiagnosticsController.php

--- net/quagga/src/opnsense/mvc/app/controllers/OPNsense/Quagga/Api/DiagnosticsController.php
+++ net/quagga/src/opnsense/mvc/app/controllers/OPNsense/Quagga/Api/DiagnosticsController.php
@@ -58,11 +58,11 @@ class DiagnosticsController extends ApiControllerBase
             $backend = new Backend();
             //$response = json_decode(trim($backend->configdRun("quagga diag-bgp2")), true);
             $response = json_decode(trim($backend->configdRun("quagga diag-bgp2")));
-            //return array("response" => $response);
-            return $response;
+            return array("response" => $response);
+            //return $response;
         } else {
-            //return array("response" => array());
-            return $response;
+            return array("response" => array());
+            //return $response;
         }
     }
     /**
