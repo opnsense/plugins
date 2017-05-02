336750c4df2d1a889d207a630518fb91872b5a11
Michael <m.muenz@gmail.com>
2017-05-02 13:45:56 +0200

    Update OSPF6.php

--- net/quagga/src/opnsense/mvc/app/models/OPNsense/Quagga/OSPF6.php
+++ net/quagga/src/opnsense/mvc/app/models/OPNsense/Quagga/OSPF6.php
@@ -19,6 +19,6 @@
     ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
     POSSIBILITY OF SUCH DAMAGE.
 */
-class OSPF extends BaseModel
+class OSPF6 extends BaseModel
 {
 }
