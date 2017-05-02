6b54447ba8e414beeb297a5681e5a9088e79cf25
Michael <m.muenz@gmail.com>
2017-05-02 22:29:19 +0200

    Update OSPF6.php

--- net/quagga/src/opnsense/mvc/app/models/OPNsense/Quagga/OSPF6.php
+++ net/quagga/src/opnsense/mvc/app/models/OPNsense/Quagga/OSPF6.php
@@ -6,6 +6,7 @@ use OPNsense\Base\BaseModel;
 
 /*
     Copyright (C) 2017 Fabian Franz
+    Copyright (C) 2017 Michael Muenz
     All rights reserved.
     Redistribution and use in source and binary forms, with or without
     modification, are permitted provided that the following conditions are met:
