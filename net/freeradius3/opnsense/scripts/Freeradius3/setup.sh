35fe0466e06f53d0fc0ba9cd2fcd356cdc5ecf20
Michael <m.muenz@gmail.com>
2017-06-30 14:41:37 +0200

    Update setup.sh

--- net/freeradius3/opnsense/scripts/Freeradius3/setup.sh
+++ net/freeradius3/opnsense/scripts/Freeradius3/setup.sh
@@ -1,1 +1,15 @@
+#!/bin/sh
 
+user=radiusd
+group=radiusd
+
+mkdir -p /var/run/radiusd
+chown $user:$group /var/run/radiusd
+chmod 750 /var/run/radiusd
+
+mkdir -p /usr/local/etc/raddb/
+chown $user:$group /usr/local/etc/raddb/
+chmod 750 /usr/local/etc/raddb/
+
+chown -R $user:$group /usr/local/etc/raddb
+chown -R $user:$group /var/run/radiusd
