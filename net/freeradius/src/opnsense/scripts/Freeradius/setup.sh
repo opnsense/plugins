0faa4914fd1809b6c9f30e5d180d813c75f0ae04
Franco Fichtner <franco@opnsense.org>
2017-07-12 07:05:10 +0200

    net/freeradius: new plugin by @mimugmail

---
+++ net/freeradius/src/opnsense/scripts/Freeradius/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+RADIUS_FILES=/var/log/radius.log /var/log/radutmp /var/log/radwtmp
+RADIUS_DIRS=/usr/local/etc/raddb /var/run/radiusd /var/log/radacct
+RADIUS_USER=freeradius
+RADIUS_GROUP=freeradius
+
+for DIR in ${RADIUS_DIRS}; do
+	mkdir -p ${DIR}
+	chmod -R 750 ${DIR}
+	chown -R ${RADIUS_USER}:${RADIUS_GROUP} ${DIR}
+done
+
+for FILE in ${RADIUS_FILES}; do
+	touch ${FILE}
+	chmod 700 ${FILE}
+done
