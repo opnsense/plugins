97d6b2b3fe4707e036416c81d6ef350e80586912
Franco Fichtner <franco@opnsense.org>
2017-07-24 10:42:03 +0200

    net/freeradius: fix another hiccup; closes #207

--- net/freeradius/src/opnsense/scripts/Freeradius/setup.sh
+++ net/freeradius/src/opnsense/scripts/Freeradius/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-RADIUS_FILES=/var/log/radius.log /var/log/radutmp /var/log/radwtmp
-RADIUS_DIRS=/usr/local/etc/raddb /var/run/radiusd /var/log/radacct
+RADIUS_FILES="/var/log/radius.log /var/log/radutmp /var/log/radwtmp"
+RADIUS_DIRS="/usr/local/etc/raddb /var/run/radiusd /var/log/radacct"
 RADIUS_USER=freeradius
 RADIUS_GROUP=freeradius
 
