c0041bfadfc979fc430b9913a095983ae022411d
Michael <m.muenz@gmail.com>
2017-08-11 06:40:33 +0200

    initial c-icap commit

---
+++ www/c-icap/src/opnsense/scripts/OPNsense/CICAP/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir -p /var/run/c-icap
+chown -R c_icap:c_icap /var/run/c-icap
+chmod 750 /var/run/c-icap
+
+mkdir -p /var/log/c-icap
+chown -R c_icap:c_icap /var/log/c-icap
+chmod 750 /var/log/c-icap
