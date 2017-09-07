f3b7044c46f3daab9f659b338933d5ba91fbf115
Frank Wall <fw@moov.de>
2017-09-07 13:37:06 +0200

    security/acme-client: write import events to system log, refs #256

--- security/acme-client/src/opnsense/scripts/OPNsense/AcmeClient/certhelper.php
+++ security/acme-client/src/opnsense/scripts/OPNsense/AcmeClient/certhelper.php
@@ -982,6 +982,7 @@ function import_certificate($certObj, $modelObj)
     // Write changes to config
     // TODO: Legacy code, should be replaced with code from OPNsense framework
     write_config("${import_log_message} Let's Encrypt SSL certificate: ${cert_cn}");
+    log_error("AcmeClient: ${import_log_message} Let's Encrypt SSL certificate: ${cert_cn}");
 
     // Update (acme) certificate object (through MVC framework)
     $uuid = $certObj->attributes()->uuid;
