56daf10188cbfe51ebffe388b5df3fbe001a6e29
Giuseppe <giuseppe.demarco@unical.it>
2017-09-18 11:39:10 +0200

    ARPscanner.py improvements. extending methods for appropriate I/O

--- net/arp-scan/src/opnsense/scripts/OPNsense/ARPscanner/ARPscanner.py
+++ net/arp-scan/src/opnsense/scripts/OPNsense/ARPscanner/ARPscanner.py
@@ -28,7 +28,11 @@
 """
 from subprocess import Popen, PIPE
 import datetime
+import os.path
+from os import kill, getpid
+import signal
 import json
+import sys
 import re
 #~ from IPtools import get_ip_address
 
@@ -51,8 +55,10 @@ RFC1918_NETWORKS = ["192.168.0.0/16",
                     "172.31.0.0/16",
                     "10.0.0.0/8"]
 
-class ArpScanner(object):    
-    def __init__(self, ifname, network_list):
+class ArpScanner(object):
+    os_command_filter = """ps ax | grep "ARPscanner\|arp-scan -I" | grep {} | grep -E '^[ 0-9]+'| awk -F' ' '{{print $1}}'"""
+    
+    def __init__(self, ifname, network_list, background=False):
         """
         netif='eth0'
         network_list=['192.168.0.0/24', ...]
@@ -63,17 +69,44 @@ class ArpScanner(object):
         self.result         = {} # filtered output containing only what needed
         # regexp used to retrieve data from arp-scan system command stdout
         self.regexp =  '([0-9\.]+)[\t]*([\dA-F]{2}(?:[-:][\dA-F]{2}){5})[\t]*([A-Za-z0-9\ \.\-\,\'\(\)]*)'
+        # os_command_filter needs '{}'.format(ifname)
+        self.mode   = background
+        self.tmp    = '/tmp/ARPscanner'
         self._DEBUG = False
     
-    @staticmethod
-    def check_run(ifname):
+    @classmethod
+    def check_run(cls, ifname):
         """
-           returns 1 if running on that ifname
+           returns PID if running on that ifname
+           else return 0
         """
         # read PID attribute of .current
         # if PID exists: 
         #    return 1
+        
+        os_command = cls.os_command_filter.format(ifname)
+        osc = Popen(os_command, 
+                      stdin=PIPE, 
+                      stdout=PIPE, 
+                      stderr=PIPE,
+                      shell=True)
+        output, err = osc.communicate()
+        if output: return output
         return 0
+    
+    @classmethod
+    def stop(cls, ifname):
+        mypid = getpid()
+        pids = [int(i) for i in cls.check_run(ifname).split('\n') if i]
+        killed = []
+        for pid in pids:
+            if pid == mypid: continue
+            try:
+                kill(pid, 9)
+                killed.append(pid)
+            except Exception as e:
+                pass
+        return killed
     
     @staticmethod
     def status(ifname):
@@ -84,8 +117,16 @@ class ArpScanner(object):
         # return (returncode, output, err)
         pass
         
-    def status(self, ifname):
+    def prepare_start(self, ifname):
         # check if tmpfolder/$ifname.current exists
+        fname = '{}.current'.format(ifname)
+        lname = '{}.last'.format(ifname)
+        fpath = os.path.sep.join((self.tmp, fname))
+        lpath = os.path.sep.join((self.tmp, lname))
+        if not os.path.exists(self.tmp):
+            os.makedirs(self.tmp)
+        if not os.path.isfile(fpath):
+            pass
         # read PID attribute of .current
         # if PID exists: 
         #    check if it is not dead: move .current to .last
@@ -94,36 +135,35 @@ class ArpScanner(object):
         # else: 
         #    write the .current status file 
         pass
-        
+    
     def run_command(self, os_command, background=False):
         """
            os_command: (list) command to run 
            background: .current and .last file are used for I/O
         """
-        
+        osc = Popen(os_command, 
+                      stdin=PIPE, 
+                      stdout=PIPE, 
+                      stderr=PIPE)
         
         if background: 
             # run a child and detach
-            
             # write json object on every stdout LINE on the file
             pass
         else:
-            osc = Popen(os_command, 
-                          stdin=PIPE, 
-                          stdout=PIPE, 
-                          stderr=PIPE)
+            # wait for stdout
             output, err = osc.communicate()
             returncode = osc.returncode
             if self._DEBUG: print(os_command, returncode, output, err)
-            return returncode, output, err
         
-        
+        return returncode, output, err
+    
     def start(self):
         self.result['networks'] = {}
         for net in self.network_list:
             os_command = ["arp-scan", "-I", self.ifname, net]
             # prepare_start(self.ifname
-            self.outputs[net] = self.run_command(os_command)
+            self.outputs[net] = self.run_command(os_command, self.mode)
             #~ self.outputs[net] = returncode, output, err
             regexp = re.findall(self.regexp , self.outputs[net][1], re.I)
             if self._DEBUG: print(regexp)
@@ -136,37 +176,50 @@ class ArpScanner(object):
                 
         self.result['interface'] = self.ifname
         self.result['datetime']  = datetime.datetime.now().isoformat()
-        
+    
     def view_outputs(self):
         for res in self.outputs:
             print(res)
             print('return code: {}\n'.format(self.outputs[res][0]))
             print(self.outputs[res][1])
-
+    
     def get_json(self):
         return json.dumps(self.result)
     
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', nargs='?', required=True, help="network interface")
+    parser.add_argument('-i', nargs='?', required=True, 
+                        help="network interface")
     parser.add_argument('-r', nargs='+', help="""multiple network ranges,
     as: 192.168.1.0/24 172.16.31.0/12
     If not specified it will scan all the RFC 1918 local area networks.""")
-    parser.add_argument('-d', action="store_true", required=False, help="background api mode")
+    parser.add_argument('-d', action="store_true", required=False, 
+                        help="background api mode")
+    parser.add_argument('-c', action="store_true", required=False, 
+                        help="check if arp-san is running on that interface")
+    parser.add_argument('-stop', action="store_true", required=False, 
+                        help="Stops scanning on that interfaces")
 
     args = parser.parse_args()
     
-    if not args.r or len(args.r[0]) == 0:
-        args.r = ['--localnet']
+    if args.r is None or len(args.r[0]) == 0:
+        args.r = ['--localnet',]
     else:
         args.r = args.r[0].split(',')
-
+    
     #~ print("Scan interface: {}".format(args.i))    
     #~ plural = '' if len(args.i) == 1 else 's'
     #~ print('Network{} to scan: {}'.format(plural, ' '.join(args.r)))
     
-    ap = ArpScanner(args.i, args.r)
+    if args.c:
+        sys.exit(ArpScanner.check_run(args.i))
+
+    if args.stop:
+        sys.exit(ArpScanner.stop(args.i))
+
+    # if args.d -> background run
+    ap = ArpScanner(args.i, args.r, 1 if args.d else 0)
     # ap.check_run()
     ap.start()
     print(ap.get_json())
