9240e565fd8d803aa57c2fdceb7f0a7fe0ac2916
Fabian Franz <fabian.franz@students.fh-hagenberg.at>
2017-09-24 17:35:43 +0200

    setup: set permissions for the log file and create it when missing

--- security/tor/src/opnsense/service/scripts/tor/setup.sh
+++ security/tor/src/opnsense/service/scripts/tor/setup.sh
@@ -6,5 +6,9 @@ mkdir -p /var/run/tor
 chown _tor:_tor /var/db/tor
 chmod 700 /var/db/tor
 
+touch /var/log/tor.log
+chmod 700 /var/log/tor.log
+chown _tor:_tor /var/log/tor.log
+
 # required to access the pf device for nat
 /usr/sbin/pw groupmod proxy -m _tor
