8b274753f9ad33f181ca914e60fa5a92d3c4913d
Fabian Franz <fabianfrz@users.noreply.github.com>
2017-09-25 08:19:50 +0200

    Tor: fix permission issue (#286)
    * setup: set permissions for the log file and create it when missing
    
    * create dirs for hidden services
    
    * Update make_hidden_service_dirs.php
    
    * Tor: add pid dir to setup.sh
    
    * add copyright header to PHP files

--- security/tor/src/opnsense/service/scripts/tor/setup.sh
+++ security/tor/src/opnsense/service/scripts/tor/setup.sh
@@ -6,5 +6,14 @@ mkdir -p /var/run/tor
 chown _tor:_tor /var/db/tor
 chmod 700 /var/db/tor
 
+touch /var/log/tor.log
+chmod 700 /var/log/tor.log
+chown _tor:_tor /var/log/tor.log
+
+chown _tor:_tor /var/run/tor
+
+# create hidden service dirs:
+/usr/local/opnsense/service/scripts/tor/make_hidden_service_dirs.php
+
 # required to access the pf device for nat
 /usr/sbin/pw groupmod proxy -m _tor
