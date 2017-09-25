ecd25348b62f0b66a90840e7489a48b866ea2f33
Franco Fichtner <franco@opnsense.org>
2017-09-25 08:05:55 +0200

    security/tor: merge version0.2 from master

--- security/tor/src/opnsense/service/scripts/tor/setup.sh
+++ security/tor/src/opnsense/service/scripts/tor/setup.sh
@@ -3,5 +3,8 @@ mkdir -p /var/db/tor
 mkdir -p /var/log/tor
 mkdir -p /var/run/tor
 
+chown _tor:_tor /var/db/tor
+chmod 700 /var/db/tor
+
 # required to access the pf device for nat
 /usr/sbin/pw groupmod proxy -m _tor
