153029ae76629763e5dfe8a6418afbff681bb473
Alexander Shursha <kekek2@ya.ru>
2017-10-10 13:53:12 +0300

    Show ClamAV version and databases versions (#317)

--- security/clamav/src/opnsense/mvc/app/controllers/OPNsense/ClamAV/GeneralController.php
+++ security/clamav/src/opnsense/mvc/app/controllers/OPNsense/ClamAV/GeneralController.php
@@ -34,6 +34,7 @@ class GeneralController extends \OPNsense\Base\IndexController
     {
         $this->view->title = gettext("ClamAV Settings");
         $this->view->generalForm = $this->getForm("general");
+        $this->view->versionForm = $this->getForm("version");
         $this->view->pick('OPNsense/ClamAV/general');
     }
 }
