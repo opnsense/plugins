70f271fe4aa8cbd9a7bf1f4a0dfbeb940d7d5b7f
Michael <m.muenz@gmail.com>
2017-10-24 11:04:27 +0200

    Update setup.sh

--- net-mgmt/zabbix-proxy/src/opnsense/scripts/OPNsense/Zabbixproxy/setup.sh
+++ net-mgmt/zabbix-proxy/src/opnsense/scripts/OPNsense/Zabbixproxy/setup.sh
@@ -11,5 +11,5 @@ chown -R zabbix:zabbix /var/log/zabbix_proxy.log
 
 # Setup PID directory
 mkdir -p /var/run/zabbix
-chown -R zabbix:zabbix /var/db/zabbix
+chown -R zabbix:zabbix /var/run/zabbix
 chmod 755 /var/run/zabbix
