af9fb77869c30930ee370d0e666bc0497c96591d
Michael <m.muenz@gmail.com>
2017-10-24 10:48:36 +0200

    Update setup.sh

--- net-mgmt/zabbix-proxy/src/opnsense/scripts/OPNsense/Zabbixproxy/setup.sh
+++ net-mgmt/zabbix-proxy/src/opnsense/scripts/OPNsense/Zabbixproxy/setup.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Setup database directory
 mkdir -p /var/db/zabbix
 chown -R zabbix:zabbix /var/db/zabbix
 chmod 755 /var/db/zabbix
+
+# Setup logging
+touch /var/log/zabbix_proxy.log
+chown -R zabbix:zabbix /var/log/zabbix_proxy.log
