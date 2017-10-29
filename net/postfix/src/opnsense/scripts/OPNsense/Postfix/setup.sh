afb65b79673f153a031993d9de8502e4c7c7f3ce
Michael <m.muenz@gmail.com>
2017-10-29 14:22:37 +0100

    temp removed permission fixes
    seems there are other defaults

--- net/postfix/src/opnsense/scripts/OPNsense/Postfix/setup.sh
+++ net/postfix/src/opnsense/scripts/OPNsense/Postfix/setup.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 
-POSTFIX_DIRS="/var/spool/postfix /var/db/postfix"
-POSTFIX_USER=postfix
-POSTFIX_GROUP=postfix
-
-for DIR in ${POSTFIX_DIRS}; do
-	mkdir -p ${DIR}
-	chmod -R 750 ${DIR}
-	chown -R ${POSTFIX_USER}:${POSTFIX_GROUP} ${DIR}
-done
-
 postmap /usr/local/etc/postfix/transport
