7bb9506630a9f5e97f40ff4909410a66fd6f9250
Franco Fichtner <franco@opnsense.org>
2017-10-29 13:04:13 +0100

    */*: whitespace sweep

--- security/tor/src/opnsense/mvc/app/controllers/OPNsense/Tor/Api/GeneralController.php
+++ security/tor/src/opnsense/mvc/app/controllers/OPNsense/Tor/Api/GeneralController.php
@@ -1,30 +1,29 @@
 <?php
-/**
- *    Copyright (C) 2017 Fabian Franz
+
+/*
+ * Copyright (C) 2017 Fabian Franz
+ * All rights reserved.
  *
- *    All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- *    Redistribution and use in source and binary forms, with or without
- *    modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
  *
- *    1. Redistributions of source code must retain the above copyright notice,
- *       this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
  *
- *    2. Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *
- *    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
- *    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
- *    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
- *    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- *    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- *    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- *    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- *    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *    POSSIBILITY OF SUCH DAMAGE.
- *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 namespace OPNsense\Tor\Api;
@@ -71,11 +70,9 @@ class GeneralController extends ApiMutableModelControllerBase
         }
         return $result;
     }
-    
-    
-    
+
     /*  Hidden service authentication  */
-    
+
     public function searchhidservauthAction()
     {
         $this->sessionClose();
@@ -86,6 +83,7 @@ class GeneralController extends ApiMutableModelControllerBase
             array('enabled', 'onion_service', 'auth_cookie')
         );
     }
+
     public function gethidservauthAction($uuid = null)
     {
         $mdl = $this->getModel();
@@ -101,6 +99,7 @@ class GeneralController extends ApiMutableModelControllerBase
         }
         return array();
     }
+
     public function addhidservauthAction()
     {
         $result = array('result' => 'failed');
@@ -126,6 +125,7 @@ class GeneralController extends ApiMutableModelControllerBase
         }
         return $result;
     }
+
     public function delhidservauthAction($uuid)
     {
 
@@ -145,6 +145,7 @@ class GeneralController extends ApiMutableModelControllerBase
         }
         return $result;
     }
+
     public function sethidservauthAction($uuid)
     {
         if ($this->request->isPost() && $this->request->hasPost('client_auth')) {
@@ -175,6 +176,7 @@ class GeneralController extends ApiMutableModelControllerBase
         }
         return array('result' => 'failed');
     }
+
     public function toggle_handler($uuid, $element)
     {
 
