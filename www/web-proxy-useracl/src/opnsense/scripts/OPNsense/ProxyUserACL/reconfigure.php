e3fa7614fd0a91971accbbcdf8bff7fe75b8739c
Alexander Shursha <kekek2@ya.ru>
2017-11-03 08:18:49 +0300

    Change ACL name

--- www/web-proxy-useracl/src/opnsense/scripts/OPNsense/ProxyUserACL/reconfigure.php
+++ www/web-proxy-useracl/src/opnsense/scripts/OPNsense/ProxyUserACL/reconfigure.php
@@ -37,9 +37,9 @@ use \OPNsense\Core\Config;
 $mdlProxyUserACL = new ProxyUserACL();
 $domain = strtoupper((string) Config::getInstance()->object()->system->domain);
 
-array_map('unlink', glob("/usr/local/etc/squid/ACL_*.txt"));
+array_map('unlink', glob("/usr/local/etc/squid/ACL_useracl_*.txt"));
 foreach ($mdlProxyUserACL->getNodeByReference('general.ACLs.ACL')->getNodes() as $acl) {
-    file_put_contents("/usr/local/etc/squid/ACL_" .
+    file_put_contents("/usr/local/etc/squid/ACL_useracl_" .
         $acl["Priority"] . ".txt", $acl["Name"] . "\n" .
         ($acl["Group"]["user"]["selected"] == "1" ? $acl["Name"] . "@" . $domain . "\n" : ""));
 }
