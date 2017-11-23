3c1ac320221c0af1eb3776cfa9330b087e5f783c
Franco Fichtner <franco@opnsense.org>
2017-11-23 20:25:48 +0100

    */*: follow-up for previous

--- www/c-icap/src/opnsense/mvc/app/controllers/OPNsense/CICAP/Api/ServiceController.php
+++ www/c-icap/src/opnsense/mvc/app/controllers/OPNsense/CICAP/Api/ServiceController.php
@@ -60,6 +60,9 @@ class ServiceController extends ApiControllerBase
     public function startAction()
     {
         if ($this->request->isPost()) {
+            // close session for long running action
+            $this->sessionClose();
+
             $backend = new Backend();
             $response = $backend->configdRun("cicap start");
             return array("response" => $response);
@@ -75,6 +78,9 @@ class ServiceController extends ApiControllerBase
     public function stopAction()
     {
         if ($this->request->isPost()) {
+            // close session for long running action
+            $this->sessionClose();
+
             $backend = new Backend();
             $response = $backend->configdRun("cicap stop");
             return array("response" => $response);
@@ -90,6 +96,9 @@ class ServiceController extends ApiControllerBase
     public function restartAction()
     {
         if ($this->request->isPost()) {
+            // close session for long running action
+            $this->sessionClose();
+
             $backend = new Backend();
             $response = $backend->configdRun("cicap restart");
             return array("response" => $response);
@@ -122,7 +131,6 @@ class ServiceController extends ApiControllerBase
         } else {
             $status = "unkown";
         }
-
 
         return array("status" => $status);
     }
