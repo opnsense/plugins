eb0eb24f94771d048dd2a4cb64264ae0b54b1fbc
Franco Fichtner <franco@opnsense.org>
2017-12-18 18:28:10 +0000

    */*: migrate _opnsense_bootup_run to simpler _var_script suffix
    PR: https://github.com/opnsense/core/issues/1835
    (cherry picked from commit f695bc25ce79df7dad6bb4201dc3dac1ff54affc)

--- net-mgmt/zabbix-agent/src/opnsense/service/templates/OPNsense/ZabbixAgent/rc.conf.d
+++ net-mgmt/zabbix-agent/src/opnsense/service/templates/OPNsense/ZabbixAgent/rc.conf.d
@@ -1,6 +1,7 @@
 {% if helpers.exists('OPNsense.ZabbixAgent.settings.main.enabled') and OPNsense.ZabbixAgent.settings.main.enabled|default("0") == "1" %}
 zabbix_agentd_enable=YES
 zabbix_agentd_opnsense_bootup_run="/usr/local/opnsense/scripts/OPNsense/ZabbixAgent/setup.sh"
+zabbix_agentd_var_script="/usr/local/opnsense/scripts/OPNsense/ZabbixAgent/setup.sh"
 {% else %}
 zabbix_agentd_enable=NO
 {% endif %}
--- security/acme-client/src/opnsense/service/templates/OPNsense/AcmeClient/rc.conf.d
+++ security/acme-client/src/opnsense/service/templates/OPNsense/AcmeClient/rc.conf.d
@@ -3,6 +3,7 @@ acme_http_challenge_enable=YES
 acme_http_challenge_conf="/var/etc/lighttpd-acme-challenge.conf"
 acme_http_challenge_pidfile="/var/run/lighttpd-acme-challenge.pid"
 acme_http_challenge_opnsense_bootup_run="/usr/local/opnsense/scripts/OPNsense/AcmeClient/setup.sh"
+acme_http_challenge_var_script="/usr/local/opnsense/scripts/OPNsense/AcmeClient/setup.sh"
 {% else %}
 acme_http_challenge_enable=NO
 {% endif %}
