0a355692e716b7585674a670795b6980673112dd
Franco Fichtner <franco@opnsense.org>
2017-12-19 07:25:18 +0100

    net/frr: zap title

--- net/frr/src/opnsense/mvc/app/controllers/OPNsense/Quagga/IsisController.php
+++ net/frr/src/opnsense/mvc/app/controllers/OPNsense/Quagga/IsisController.php
@@ -31,7 +31,6 @@ class IsisController extends \OPNsense\Base\IndexController
 {
     public function indexAction()
     {
-        $this->view->title = gettext("IS-IS-Settings");
         $this->view->generalForm = $this->getForm("isis");
         $this->view->pick('OPNsense/Quagga/isis');
     }
