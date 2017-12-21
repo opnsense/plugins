83542e9c11aa30fb18b60e9713224454da55fe01
Alexander Shursha <kekek2@ya.ru>
2017-12-21 11:07:44 +0300

    Controllers simplification.

--- www/c-icap/src/opnsense/mvc/app/controllers/OPNsense/CICAP/Api/GeneralController.php
+++ www/c-icap/src/opnsense/mvc/app/controllers/OPNsense/CICAP/Api/GeneralController.php
@@ -31,47 +31,10 @@
 
 namespace OPNsense\CICAP\Api;
 
-use \OPNsense\Base\ApiControllerBase;
-use \OPNsense\CICAP\General;
-use \OPNsense\Core\Config;
+use \OPNsense\Base\ApiMutableModelControllerBase;
 
-class GeneralController extends ApiControllerBase
+class GeneralController extends ApiMutableModelControllerBase
 {
-    public function getAction()
-    {
-        // define list of configurable settings
-        $result = array();
-        if ($this->request->isGet()) {
-            $mdlGeneral = new General();
-            $result['general'] = $mdlGeneral->getNodes();
-        }
-        return $result;
-    }
-
-    public function setAction()
-    {
-        $result = array("result"=>"failed");
-        if ($this->request->isPost()) {
-            // load model and update with provided data
-            $mdlGeneral = new General();
-            $mdlGeneral->setNodes($this->request->getPost("general"));
-
-            // perform validation
-            $valMsgs = $mdlGeneral->performValidation();
-            foreach ($valMsgs as $field => $msg) {
-                if (!array_key_exists("validations", $result)) {
-                    $result["validations"] = array();
-                }
-                $result["validations"]["general.".$msg->getField()] = $msg->getMessage();
-            }
-
-            // serialize model to config and save
-            if ($valMsgs->count() == 0) {
-                $mdlGeneral->serializeToConfig();
-                Config::getInstance()->save();
-                $result["result"] = "saved";
-            }
-        }
-        return $result;
-    }
+    static protected $internalModelName = 'general';
+    static protected $internalModelClass = '\OPNsense\CICAP\General';
 }
