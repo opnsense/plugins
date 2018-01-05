293804878e5833514eccd684afc92fd837c79f5e
NOYB <Al_Stu@Frontier.com>
2018-01-04 20:21:38 -0800

    HTML Compliance - Attribute "type" on Element <i>
    Error: Attribute "type" not allowed on element <i> at this point.
    (cherry picked from commit 9960e5a6c13c4ad351aaa3df9682938f8fa65a0e)

--- net/pppoe/src/www/vpn_pppoe_edit.php
+++ net/pppoe/src/www/vpn_pppoe_edit.php
@@ -269,7 +269,7 @@ legacy_html_escape_form_data($pconfig);
                     <td style="width:22%"><strong><?=gettext("PPPoE server configuration");?></strong></td>
                     <td style="width:78%; text-align:right">
                       <small><?=gettext("full help"); ?> </small>
-                      <i class="fa fa-toggle-off text-danger"  style="cursor: pointer;" id="show_all_help_page" type="button"></i>
+                      <i class="fa fa-toggle-off text-danger"  style="cursor: pointer;" id="show_all_help_page"></i>
                     </td>
                   </tr>
                   <tr>
