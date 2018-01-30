8ebb3bd46593d7c518c2f8f5373dca7569945b6e
Franco Fichtner <franco@opnsense.org>
2018-01-30 10:22:18 +0100

    */*: remove _opnsense_bootup_run
    PR: https://github.com/opnsense/core/issues/1835

--- net/haproxy/src/opnsense/service/templates/OPNsense/HAProxy/rc.conf.d
+++ net/haproxy/src/opnsense/service/templates/OPNsense/HAProxy/rc.conf.d
@@ -1,6 +1,5 @@
 {% if helpers.exists('OPNsense.HAProxy.general.enabled') and OPNsense.HAProxy.general.enabled|default("0") == "1" %}
 haproxy_enable=YES
-haproxy_opnsense_bootup_run="/usr/local/opnsense/scripts/OPNsense/HAProxy/setup.sh"
 haproxy_var_script="/usr/local/opnsense/scripts/OPNsense/HAProxy/setup.sh"
 haproxy_pidfile="/var/run/haproxy.pid"
 haproxy_config="/usr/local/etc/haproxy.conf"
