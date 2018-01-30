adf9e5009d188f0efc0a009d0c17028aa73df69c
Franco Fichtner <franco@opnsense.org>
2018-01-30 10:22:18 +0100

    */*: remove _opnsense_bootup_run
    PR: https://github.com/opnsense/core/issues/1835
    (cherry picked from commit 8ebb3bd46593d7c518c2f8f5373dca7569945b6e)

--- net-mgmt/zabbix-agent/src/opnsense/service/templates/OPNsense/ZabbixAgent/rc.conf.d
+++ net-mgmt/zabbix-agent/src/opnsense/service/templates/OPNsense/ZabbixAgent/rc.conf.d
@@ -1,8 +1,7 @@
 {% if helpers.exists('OPNsense.ZabbixAgent.settings.main.enabled') and OPNsense.ZabbixAgent.settings.main.enabled|default("0") == "1" %}
-zabbix_agentd_enable=YES
-zabbix_agentd_opnsense_bootup_run="/usr/local/opnsense/scripts/OPNsense/ZabbixAgent/setup.sh"
 zabbix_agentd_var_script="/usr/local/opnsense/scripts/OPNsense/ZabbixAgent/setup.sh"
+zabbix_agentd_config="/usr/local/etc/zabbix_agentd.conf"
+zabbix_agentd_enable="YES"
 {% else %}
-zabbix_agentd_enable=NO
+zabbix_agentd_enable="NO"
 {% endif %}
-zabbix_agentd_config=/usr/local/etc/zabbix_agentd.conf
