ce0975c5328501dc97d2d13c9e529ee10679dede
Franco Fichtner <franco@opnsense.org>
2018-03-21 21:34:45 +0100

    net/relayd: fix controller/view issues like monit

--- net/relayd/src/opnsense/mvc/app/controllers/OPNsense/Relayd/Api/SettingsController.php
+++ net/relayd/src/opnsense/mvc/app/controllers/OPNsense/Relayd/Api/SettingsController.php
@@ -72,8 +72,8 @@ class SettingsController extends ApiControllerBase
                 }
             }
             if ($node != null) {
-                $result[$nodeType] = $node->getNodes();
-                $result["result"] = "ok";
+                $result['relayd'] = array($nodeType => $node->getNodes());
+                $result['result'] = 'ok';
             }
         }
         return $result;
