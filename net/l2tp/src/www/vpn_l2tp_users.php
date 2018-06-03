7f09104737c39168b02cdc842781d49e3eaa867e
Franco Fichtner <franco@opnsense.org>
2018-06-03 18:22:38 +0000

    net/l2tp: style update

--- net/l2tp/src/www/vpn_l2tp_users.php
+++ net/l2tp/src/www/vpn_l2tp_users.php
@@ -54,7 +54,7 @@ $service_hook = 'l2tpd';
 
 include("head.inc");
 $main_buttons = array(
-    array('label'=>gettext("add user"), 'href'=>'vpn_l2tp_users_edit.php'),
+    array('label' => gettext('Add'), 'href' => 'vpn_l2tp_users_edit.php'),
 );
 
 ?>
@@ -109,7 +109,7 @@ $main_buttons = array(
                   <tr>
                     <td><?=gettext("Username");?></td>
                     <td><?=gettext("IP address");?></td>
-                    <td></td>
+                    <td class="text-nowrap"></td>
                   </tr>
 <?php
                   $i = 0;
@@ -123,9 +123,9 @@ $main_buttons = array(
                       } ?>
                       <?=htmlspecialchars($secretent['ip']);?>&nbsp;
                     </td>
-                    <td>
-                      <a href="vpn_l2tp_users_edit.php?id=<?=$i;?>" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-edit"></span></a>
-                      <button data-id="<?=$i;?>" type="button" class="act_delete_user btn btn-xs btn-default"><span class="fa fa-trash text-muted"></span></button>
+                    <td class="text-nowrap">
+                      <a href="vpn_l2tp_users_edit.php?id=<?=$i;?>" class="btn btn-xs btn-default"><i class="fa fa-pencil fa-fw"></i></a>
+                      <button data-id="<?=$i;?>" type="button" class="act_delete_user btn btn-xs btn-default"><i class="fa fa-trash fa-fw"></i></button>
                     </td>
                   </tr>
 <?php
