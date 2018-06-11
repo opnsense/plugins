725bd0f6b52784abcb92318878c2350114e51f2d
Franco Fichtner <franco@opnsense.org>
2018-06-11 09:03:18 +0200

    Templates: tweaks from master

--- Templates/rc.loader.d
+++ Templates/rc.loader.d
@@ -1,2 +1,1 @@
-echo "Reloading firmware configuration"
-/usr/local/etc/rc.configure_firmware
+if [ -f /usr/local/etc/rc.configure_firmware ]; then echo "Reloading firmware configuration"; /usr/local/etc/rc.configure_firmware; fi
