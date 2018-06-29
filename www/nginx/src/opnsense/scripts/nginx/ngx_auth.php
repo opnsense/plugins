c119491dca0632366e3db72bd44a89af06183f21
Fabian Franz <franz.fabian.94@gmail.com>
2018-06-29 20:37:00 +0200

    www/nginx: fix a bug that prevents nginx starting and pass uuid of server to backend script

--- www/nginx/src/opnsense/scripts/nginx/ngx_auth.php
+++ www/nginx/src/opnsense/scripts/nginx/ngx_auth.php
@@ -1,6 +1,9 @@
 <?php
 $uri = $_SERVER['Original-URI'];
+$host = $_SERVER['Original-HOST'];
+$method = $_SERVER['Original-METHOD'];
 $is_https = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on';
+$server_uuid = $_SERVER['SERVER-UUID'];
 
 if (stristr($uri, 'pass')) {
     header("HTTP/1.1 200 OK");
