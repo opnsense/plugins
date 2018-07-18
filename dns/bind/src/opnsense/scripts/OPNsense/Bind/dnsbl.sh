5e7c6e8373345de4fb1bbff34c1724f4d20515ce
Michael <m.muenz@gmail.com>
2018-07-18 11:16:30 +0200

    Update dnsbl.sh

--- dns/bind/src/opnsense/scripts/OPNsense/Bind/dnsbl.sh
+++ dns/bind/src/opnsense/scripts/OPNsense/Bind/dnsbl.sh
@@ -1,51 +1,103 @@
 #!/bin/sh
 
-DIR="/usr/local/etc/namedb/raw/"
+# Copyright (c) 2018 Michael Muenz <m.muenz@gmail.com>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
 
-if [ ! -d $DIR ]; then
+SORT=/usr/bin/sort
+UNIQ=/usr/bin/uniq
+CD=/usr/bin/cd
+CAT=/bin/cat
+AWK=/usr/bin/awk
+FETCH="/usr/bin/fetch -qT 5"
+RM="/bin/rm -f"
 
-  mkdir $DIR
-  chown bind:bind $DIR
-  chmod -R 755 $DIR
+RAWDIR="/usr/local/etc/namedb/raw/"
+WORKDIR="/usr/local/etc/namedb/"
 
-fi
+/bin/mkdir -p $RAWDIR
+/usr/sbin/chown bind:bind $RAWDIR
+/bin/chmod -R 755 $RAWDIR
 
-cd $DIR
 
 # Download all lists:
 
 # EasyList:
-fetch -q https://justdomains.github.io/blocklists/lists/easylist-justdomains.txt -o easylist-raw
+easylist() {
+$CD $RAWDIR
+${FETCH} https://justdomains.github.io/blocklists/lists/easylist-justdomains.txt -o easylist-raw
 sed "/\.$/d" easylist-raw > easylist
-rm -f easylist-raw
+${RM} easylist-raw
+}
 
 # EasyPrivacy:
-fetch -q https://justdomains.github.io/blocklists/lists/easyprivacy-justdomains.txt -o easyprivacy
+easyprivacy() {
+$CD $RAWDIR
+${FETCH} https://justdomains.github.io/blocklists/lists/easyprivacy-justdomains.txt -o easyprivacy-raw
+sed "/\.$/d" easyprivacy-raw > easyprivacy
+${RM} easyprivacy-raw
+}
 
 # AdGuard:
-fetch -q https://justdomains.github.io/blocklists/lists/adguarddns-justdomains.txt -o adguard-raw
+adguard() {
+$CD $RAWDIR
+${FETCH} https://justdomains.github.io/blocklists/lists/adguarddns-justdomains.txt -o adguard-raw
 sed "/\.$/d" adguard-raw > adguard
-rm -f adguard-raw
+${RM} adguard-raw
+}
 
 # NoCoin:
-fetch -q https://justdomains.github.io/blocklists/lists/nocoin-justdomains.txt -o nocoin
+nocoin() {
+$CD $RAWDIR
+${FETCH} https://justdomains.github.io/blocklists/lists/nocoin-justdomains.txt -o nocoin
+}
 
 # RansomWare Tracker abuse.ch:
-fetch -q https://ransomwaretracker.abuse.ch/downloads/RW_DOMBL.txt -o rwtracker-comments
+rwtracker() {
+$CD $RAWDIR
+${FETCH} https://ransomwaretracker.abuse.ch/downloads/RW_DOMBL.txt -o rwtracker-comments
 sed '/^#/ d' rwtracker-comments > rwtracker
-rm -f rwtracker-comments
+${RM} rwtracker-comments
+}
 
 # MalwareDomains:
-fetch -q http://malwaredomains.lehigh.edu/files/justdomains -o malwaredomains-comments
+mwdomains() {
+$CD $RAWDIR
+${FETCH} http://malwaredomains.lehigh.edu/files/justdomains -o malwaredomains-comments
 sed '/^#/ d' malwaredomains-comments > malwaredomains
-rm -f malwaredomains-comments
+${RM} malwaredomains-comments
+}
 
 # Put all files in correct format
-FILES=`ls -1 /usr/local/etc/namedb/raw/`
+convert() {
+$CD $RAWDIR
+FILES=`ls -1`
 
 for i in $FILES; do
-  awk '{ print "zone " $1 " " $2 " {type master; file \"/usr/local/etc/namedb/master/blacklist.db\"; };" }' $i | sort | uniq > /usr/local/etc/namedb/$i.inc
+  $AWK '{ print "zone " $1 " " $2 " {type master; file \"/usr/local/etc/namedb/master/blacklist.db\"; };" }' $i | $SORT | $UNIQ > $WORKDIR/$i.inc
 done
+}
 
 # Depending on the options
 RETVAL=0
@@ -55,13 +107,26 @@ case "$1" in
       RETVAL=1
       ;;
    ad)
-      cd /usr/local/etc/namedb/ && cat easylist.inc easyprivacy.inc adguard.inc | sort | uniq > all.inc
+      easylist
+      easyprivacy
+      nocoin
+      convert
+      $CAT $WORKDIR/easylist.inc $WORKDIR/easyprivacy.inc $WORKDIR/adguard.inc | $SORT | $UNIQ > $WORKDIR/all.inc
       ;;
    mw)
-      cd /usr/local/etc/namedb/ && cat nocoin.inc rwtracker.inc malwaredomains.inc | sort | uniq > all.inc
+      rwtracker
+      mwdomains
+      convert
+      $CAT $WORKDIR/nocoin.inc $WORKDIR/rwtracker.inc $WORKDIR/malwaredomains.inc | $SORT | $UNIQ > $WORKDIR/all.inc
       ;;
    all)
-      cd /usr/local/etc/namedb/ && cat easylist.inc easyprivacy.inc adguard.inc nocoin.inc rwtracker.inc malwaredomains.inc | sort | uniq > all.inc
+      easylist
+      easyprivacy
+      nocoin
+      rwtracker
+      mwdomains
+      convert
+      $CAT $WORKDIR/easylist.inc $WORKDIR/easyprivacy.inc $WORKDIR/adguard.inc $WORKDIR/nocoin.inc $WORKDIR/rwtracker.inc $WORKDIR/malwaredomains.inc | $SORT | $UNIQ > $WORKDIR/all.inc
       ;;
 esac
 exit $RETVAL
