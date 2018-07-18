c260ed873d6517753fdf69f7897b5c66db457220
Michael <m.muenz@gmail.com>
2018-07-18 06:35:25 +0200

    Update GeneralController.php

--- dns/bind/src/opnsense/mvc/app/controllers/OPNsense/Bind/GeneralController.php
+++ dns/bind/src/opnsense/mvc/app/controllers/OPNsense/Bind/GeneralController.php
@@ -33,6 +33,7 @@ class GeneralController extends \OPNsense\Base\IndexController
     public function indexAction()
     {
         $this->view->generalForm = $this->getForm("general");
+        $this->view->dnsblForm = $this->getForm("dnsbl");
         $this->view->formDialogEditBindAcl = $this->getForm("dialogEditBindAcl");
         $this->view->pick('OPNsense/Bind/general');
     }
