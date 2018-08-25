46de7245edf847590af9a952f367a7de6d1df74b
Fabian Franz <franz.fabian.94@gmail.com>
2018-08-25 21:58:04 +0200

    www/nginx: add filter functionality to log view

--- www/nginx/src/opnsense/www/js/nginx/src/controller/LogView.js
+++ www/nginx/src/opnsense/www/js/nginx/src/controller/LogView.js
@@ -8,13 +8,15 @@ const LogView = Backbone.View.extend({
     tagName: 'div',
     className: 'content-box tab-content',
     events: {
-        "click .mainentry": "mainMenuClick"
+        "focusout .filter input": "update_filter"
     },
 
     initialize: function() {
         this.collection = new LogLinesCollection();
+        this.filter_model = new Backbone.Model();
         this.listenTo(this.collection, "sync", this.render);
         this.listenTo(this.collection, "update", this.render);
+        this.listenTo(this.filter_model, "change", this.render);
         this.type = '';
     },
 
@@ -22,8 +24,8 @@ const LogView = Backbone.View.extend({
         this.$el.html('');
         let table_content = '';
         let template = this.get_template();
-        this.collection.forEach((model) => table_content += template({model: model}));
-        this.$el.html(logViewer({table_body: table_content, log_type: this.type}));
+        this.filter_collection().forEach((model) => table_content += template({model: model}));
+        this.$el.html(logViewer({table_body: table_content, log_type: this.type, model: this.filter_model}));
     },
     get_template: function() {
         if (this.type === 'accesses') {
@@ -32,14 +34,41 @@ const LogView = Backbone.View.extend({
             return errorLogLine;
         }
     },
+    filter_collection: function() {
+        const that = this;
+        const filter_model = that.filter_model;
+        const filter_model_keys = that.filter_model.keys();
+        console.log(filter_model);
+        return this.collection.filter(function (model) {
+            if (!model) {
+                return false;
+            }
+            for (let i = 0; i < filter_model_keys.length; i++) {
+                const property = filter_model_keys[i];
+                if (typeof(that.filter_model.get(property)) !== "string"
+                    || that.filter_model.get(property).length === 0) {
+                    continue;
+                }
+                if (!model.get(property).includes(that.filter_model.get(property))) {
+                    return false;
+                }
+            }
+            return true;
+        });
+    },
     get_log: function(type, uuid) {
         this.collection.uuid = uuid;
         this.collection.logType = type;
         this.type = type;
+        this.filter_model.clear();
         this.update();
     },
     update: function () {
         this.collection.fetch();
+    },
+    update_filter: function (event) {
+        const element = event.target;
+        this.filter_model.set(element.name, $(element).val());
     }
 });
 export default LogView;
