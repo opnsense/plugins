a584a7958edeaf75241f95e548505154f2d74e46
alec <alecsamuelarmbruster@gmail.com>
2018-08-30 10:39:56 -0700

    Update SettingsController.php

--- dns/UnboundBL/src/opnsense/mvc/app/controllers/OPNsense/UnboundBL/Api/SettingsController.php
+++ dns/UnboundBL/src/opnsense/mvc/app/controllers/OPNsense/UnboundBL/Api/SettingsController.php
@@ -1,39 +1,8 @@
 <?php
 namespace OPNsense\UnboundBL\Api;
-
-use \OPNsense\Base\ApiControllerBase;
-use \OPNsense\UnboundBL\UnboundBL;
-use \OPNsense\Core\Config;
-class SettingsController extends ApiControllerBase
+use OPNsense\Base\ApiMutableModelControllerBase;
+class SettingsController extends ApiMutableModelControllerBase
 {
-    public function getAction()
-    {
-        $result = array();
-        if ($this->request->isGet()) {
-            $mdlUnboundBL = new UnboundBL();
-            $result['UnboundBL'] = $mdlUnboundBL->getNodes();
-        }
-        return $result;
-    }
-    public function setAction()
-    {
-        $result = array("result"=>"failed");
-        if ($this->request->isPost()) {
-            $mdlUnboundBL = new UnboundBL();
-            $mdlUnboundBL->setNodes($this->request->getPost("UnboundBL"));
-            $valMsgs = $mdlUnboundBL->performValidation();
-            foreach ($valMsgs as $field => $msg) {
-                if (!array_key_exists("validations", $result)) {
-                    $result["validations"] = array();
-                }
-                $result["validations"]["UnboundBL.".$msg->getField()] = $msg->getMessage();
-            }
-            if ($valMsgs->count() == 0) {
-                $mdlUnboundBL->serializeToConfig();
-                Config::getInstance()->save();
-                $result["result"] = "saved";
-            }
-        }
-        return $result;
-    }
+    static protected $internalModelClass = '\OPNsense\UnboundBL\Settings';
+    static protected $internalModelName = 'settings';
 }
