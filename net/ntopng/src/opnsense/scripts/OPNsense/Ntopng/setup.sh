a4dbfc26b45533e2a8e75ecfbdb26e3e445bcba7
Michael <m.muenz@gmail.com>
2018-09-07 14:45:49 +0200

    net/ntopng: new plugin (#801)
    (cherry picked from commit aaf93801a41303dac05fadf75f78d9776efeaf6d)
    (cherry picked from commit 5626cc1e8fab8c623ac15b89d9c31ad562cec9fc)

---
+++ net/ntopng/src/opnsense/scripts/OPNsense/Ntopng/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir -p /var/run/ntopng/
+chmod 755 /var/run/ntopng
+chown ntopng:ntopng /var/run/ntopng
+
+mkdir -p /var/tmp/ntopng/
+chmod 755 /var/tmp/ntopng
+chown ntopng:wheel /var/tmp/ntopng
