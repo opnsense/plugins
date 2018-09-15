2ae15e4510aa6c24d58842d0a7d291dffffffb1a
Franco Fichtner <franco@opnsense.org>
2018-09-15 15:21:57 +0200

    dns/bind: style and fixes

--- dns/bind/src/www/diag_logs_bind.php
+++ dns/bind/src/www/diag_logs_bind.php
@@ -7,13 +7,13 @@ if (isset($_GET['type']) && ($_GET['type'] === 'named' || $_GET['type'] === 'que
 
 $logfile = "/var/log/named/{$type}.log";
 $logclog = false;
-$logsplit = 5;
+$logsplit = 2;
 
 $logpills = array();
 $logpills[] = array(gettext('General'), true, '/diag_logs_bind.php?type=named');
 $logpills[] = array(gettext('Queries'), false, '/diag_logs_bind.php?type=query');
 $logpills[] = array(gettext('Blocked'), false, '/diag_logs_bind.php?type=rpz');
 
-$service_hook = 'bind';
+$service_hook = 'named';
 
 require_once 'diag_logs_template.inc';
