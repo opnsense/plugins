6a7fd14e789afd0ec8f773824068c693e443ccfb
Franco Fichtner <franco@opnsense.org>
2018-09-16 11:50:54 +0200

    *: no more trailing dots, Shrew Soft complete copyright
    (cherry picked from commit 94f4b165b11f1ae37082cc0e52c04a871fad9072)

--- net/upnp/src/www/status_upnp.php
+++ net/upnp/src/www/status_upnp.php
@@ -2,7 +2,7 @@
 
 /*
     Copyright (C) 2014-2016 Deciso B.V.
-    Copyright (C) 2010 Seth Mos <seth.mos@dds.nl>.
+    Copyright (C) 2010 Seth Mos <seth.mos@dds.nl>
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
