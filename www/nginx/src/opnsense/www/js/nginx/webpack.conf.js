cf48ba156bfa29e3c2eae076a2dee6f709362c51
Fabian Franz <franz.fabian.94@gmail.com>
2018-11-10 16:50:06 +0100

    www/nginx: check in the production version, this is smaller

--- www/nginx/src/opnsense/www/js/nginx/webpack.conf.js
+++ www/nginx/src/opnsense/www/js/nginx/webpack.conf.js
@@ -9,7 +9,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js'
     },
-    mode: 'development',
+    mode: 'production',
     module: {
         rules: [
             {
