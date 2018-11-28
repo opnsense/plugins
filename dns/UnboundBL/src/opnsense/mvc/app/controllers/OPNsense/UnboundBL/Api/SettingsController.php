6a201dbcde6f3939f5de70cfe1a8270dc8eb7153
Alec Armbruster <alecsamuelarmbruster@gmail.com>
2018-11-28 11:38:55 -0800

    [WIP] OPNblock, first dev version (#808)
    * Add files via upload
    * Update index.volt
    * Update +POST_INSTALL
    * camel-case added to class
    * Update opnblock.xml
    * Update SettingsController.php
    * Update index.volt
    * Update index.volt
    * Update opnblock.sh
    * Update IndexController.php
    * Update index.volt
    * Delete +POST_INSTALL
    * Update ACL.xml
    * Update Menu.xml
    * Update index.volt
    * Update +TARGETS
    * Update ServiceController.php
    * Update index.volt
    * Create rc.conf.d
    * Update +TARGETS
    * Rename rc.conf.d to rc.conf
    * Update +TARGETS
    * Update opnblock.sh
    * Update opnblock.xml
    * Update index.volt
    * Update index.volt
    * Update general.xml
    * Update general.xml
    * Update SettingsController.php
    * Update ServiceController.php
    * Update SettingsController.php
    * Update general.xml
    * Update opnblock.conf
    * Update SettingsController.php
    * Update index.volt
    * Update ServiceController.php
    * Update index.volt
    * Update IndexController.php
    * Update index.volt
    * Create +POST_INSTALL
    * Update +POST_INSTALL
    * Update opnblock.sh
    * Update opnblock.xml
    * Update general.xml
    * Update rc.conf
    * Update opnblock.conf
    * Update general.xml
    * Update ServiceController.php
    * Delete pkg-descr
    * Delete Makefile
    * Delete +POST_INSTALL
    * Delete rc.conf
    * Delete opnblock.conf
    * Delete +TARGETS
    * Delete actions_opnblock.conf
    * Delete opnblock.sh
    * Delete index.volt
    * Delete opnblock.xml
    * Delete opnblock.php
    * Delete Menu.xml
    * Delete ACL.xml
    * Delete IndexController.php
    * Delete general.xml
    * Delete SettingsController.php
    * Delete ServiceController.php
    * Add files via upload
    * Update Menu.xml
    * Update UnboundBL.xml
    * Update general.xml
    * Update index.volt
    * Update UnboundBL.xml
    * Update IndexController.php
    * Update ServiceController.php
    * Update ServiceController.php
    * Update ServiceController.php
    * Update ServiceController.php
    * Update SettingsController.php
    * Update Menu.xml
    * Update SettingsController.php
    * Delete +POST_INSTALL
    * Update ServiceController.php
    * Update ServiceController.php
    * Update UnboundBL.xml
    * Update rc.conf
    * Update actions_UnboundBL.conf
    * Update ServiceController.php
    * Update rc.conf
    * Update rc.conf

---
+++ dns/UnboundBL/src/opnsense/mvc/app/controllers/OPNsense/UnboundBL/Api/SettingsController.php
@@ -0,0 +1,8 @@
+<?php
+namespace OPNsense\UnboundBL\Api;
+use OPNsense\Base\ApiMutableModelControllerBase;
+class SettingsController extends ApiMutableModelControllerBase
+{
+    static protected $internalModelClass = '\OPNsense\UnboundBL';
+    static protected $internalModelName = 'UnboundBL';
+}
