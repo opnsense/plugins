b8d15a8b50b2b723830eb2d2c0117668f67f3420
Fabian Franz <franz.fabian.94@gmail.com>
2018-12-19 19:19:58 +0100

    www/nginx: add minimal paging for not crashing on a 40 MiB file

--- www/nginx/src/opnsense/www/js/nginx/src/controller/LogView.js
+++ www/nginx/src/opnsense/www/js/nginx/src/controller/LogView.js
@@ -32,15 +32,21 @@ const LogView = Backbone.View.extend({
     tagName: 'div',
     className: 'content-box tab-content',
     events: {
-        "keyup .filter input": "update_filter"
+        "keyup .filter input": "update_filter",
+        "click #paging_back": "page_back",
+        "click #paging_forward": "page_forward",
+        "change #entrycount": "change_entry_count",
     },
+    page_entry_count: 100,
+    current_page: 0,
+    current_filtered_collection: null,
 
     initialize: function() {
         this.collection = new LogLinesCollection();
         this.filter_model = new Backbone.Model();
-        this.listenTo(this.collection, "sync", this.render);
-        this.listenTo(this.collection, "update", this.render);
-        this.listenTo(this.filter_model, "change", this.render);
+        this.listenTo(this.collection, "sync", this.clear_and_render);
+        this.listenTo(this.collection, "update", this.clear_and_render);
+        this.listenTo(this.filter_model, "change", this.clear_and_render);
         this.type = '';
     },
 
@@ -58,7 +64,12 @@ const LogView = Backbone.View.extend({
             tbody.html('');
         }
         if (this.collection.length !== 0) {
-            this.collection.filter_collection(this.filter_model).forEach(
+            if (this.current_filtered_collection == null) {
+                this.current_filtered_collection = this.collection.filter_collection(this.filter_model);
+            }
+            const index_begin = this.current_page * this.page_entry_count;
+            const index_end = index_begin + this.page_entry_count;
+            this.current_filtered_collection.slice(index_begin, index_end).forEach(
                 (model) => this.render_one(tbody, model)
             );
         }
@@ -79,9 +90,30 @@ const LogView = Backbone.View.extend({
     update: function () {
         this.collection.fetch();
     },
+    clear_and_render: function() {
+        this.current_filtered_collection = null;
+        this.render();
+    },
     update_filter: function (event) {
         const element = event.target;
         this.filter_model.set(element.name, $(element).val());
+    },
+    page_back: function () {
+        if (this.current_page > 0) {
+            this.current_page--;
+            this.render();
+        }
+    },
+    page_forward: function () {
+        if ((this.current_page + 1) * this.page_entry_count < this.collection.length) {
+            this.current_page++;
+            this.render();
+        }
+    },
+    change_entry_count: function (event) {
+        this.page_entry_count = event.target.value;
+        this.current_page = 0;
+        this.render();
     }
 });
 export default LogView;
