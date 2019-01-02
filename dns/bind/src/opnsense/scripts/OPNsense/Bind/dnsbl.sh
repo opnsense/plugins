e005172f59da7a06cd2a42dcc067f806acbad425
Franco Fichtner <franco@opnsense.org>
2019-01-02 10:44:02 +0100

    dns/bind: release 1.4

--- dns/bind/src/opnsense/scripts/OPNsense/Bind/dnsbl.sh
+++ dns/bind/src/opnsense/scripts/OPNsense/Bind/dnsbl.sh
@@ -126,6 +126,41 @@ yoyo() {
 	rm ${WORKDIR}/yoyo-raw
 }
 
+stevenblack() {
+        # StevenBlack
+        ${FETCH} https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts -o ${WORKDIR}/stevenblack-raw
+        sed "/\.$/d" ${WORKDIR}/stevenblack-raw | sed "/^#/d" | sed "/\_/d" | sed "/^\s*$/d" | sed "/\.\./d" | sed "s/^\.//g" | sed "/localhost/d" | sed "/127\.0\.0\.1/d" | sed "/255\.255\.255\.255/d" | sed "/\:\:1/d" | sed "/fe80\:\:1/d" | sed "/ff00\:\:/d" | sed "/ff02\:\:/d" | sed "/0\.0\.0\.0 0\.0\.0\.0/d" | tr -d '\r' | awk 'BEGIN{FS=OFS=" ";}{print $2;}' > ${WORKDIR}/stevenblack
+        rm ${WORKDIR}/stevenblack-raw
+}
+
+hbbtv() {
+	# HBBTV List
+	${FETCH} https://raw.githubusercontent.com/Akamaru/Pi-Hole-Lists/master/hbbtv.txt -o ${WORKDIR}/hbbtv-raw
+	sed "/\.$/d" ${WORKDIR}/hbbtv-raw | sed "/^#/d" | sed "/\_/d" | sed "/^\s*$/d" | sed "/\.\./d" | sed "s/^\.//g" > ${WORKDIR}/hbbtv
+	rm ${WORKDIR}/hbbtv-raw
+}
+
+simplead() {
+	# Simple Ad List
+	${FETCH} https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt -o ${WORKDIR}/simplead-raw
+	sed "/\.$/d" ${WORKDIR}/simplead-raw | sed "/^#/d" | sed "/\_/d" | sed "/^\s*$/d" | sed "/\.\./d" | sed "s/^\.//g" > ${WORKDIR}/simplead
+	rm ${WORKDIR}/simplead-raw
+}
+
+simpletrack() {
+	# Simple Tracking List
+	${FETCH} https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt -o ${WORKDIR}/simpletrack-raw
+	sed "/\.$/d" ${WORKDIR}/simpletrack-raw | sed "/^#/d" | sed "/\_/d" | sed "/^\s*$/d" | sed "/\.\./d" | sed "s/^\.//g" > ${WORKDIR}/simpletrack
+	rm ${WORKDIR}/simpletrack-raw
+}
+
+zeusabuse() {
+	# Zeus Tracker List from abuse.ch
+	${FETCH} https://zeustracker.abuse.ch/blocklist.php?download=domainblocklist -o ${WORKDIR}/zeusabuse-raw
+	sed "/\.$/d" ${WORKDIR}/zeusabuse-raw | sed "/^#/d" | sed "/\_/d" | sed "/^\s*$/d" | sed "/\.\./d" | sed "s/^\.//g" > ${WORKDIR}/zeusabuse
+	rm ${WORKDIR}/zeusabuse-raw
+}
+
 install() {
 	# Put all files in correct format
 	for FILE in $(find ${WORKDIR} -type f); do
@@ -164,6 +199,9 @@ for CAT in $(echo ${DNSBL} | tr ',' ' '); do
 	emd)
 		emdlist
 		;;
+	ht)
+		hbbtv
+		;;
 	nc)
 		nocoin
 		;;
@@ -179,11 +217,23 @@ for CAT in $(echo ${DNSBL} | tr ',' ' '); do
 	pt)
 		porntop
 		;;
+	sa)
+		simplead
+		;;
+	sb)
+		stevenblack
+		;;
+	st)
+		simpletrack
+		;;
 	ws)
 		windowsspyblocker
 		;;
 	yy)
 		yoyo
+		;;
+	za)
+		zeusabuse
 		;;
 	esac
 done
