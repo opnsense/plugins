a2845908fb8970250671a2afbdee4a0c0346e7a5
opnsenseuser <rene@team-rebellion.net>
2019-01-19 15:54:33 +0100

    dyndns widget - fix color status for success/warning instead of green/red
    the ip color of the dyndns widget is now automatically adjusted for each theme. (text-success or text-warning). This results in a nicer overall picture and does not use another color value for green or red.

--- dns/dyndns/src/www/widgets/widgets/dyn_dns_status.widget.php
+++ dns/dyndns/src/www/widgets/widgets/dyn_dns_status.widget.php
@@ -64,16 +64,16 @@ if (!empty($_REQUEST['getdyndnsstatus'])) {
             $cached_ip_s = explode('|', $fdata);
             $cached_ip = $cached_ip_s[0];
             echo sprintf(
-                '<font color="%s">%s</font>',
-                $ipaddr != $cached_ip ? 'red' : 'green',
+                '<font class="%s">%s</font>',
+                $ipaddr != $cached_ip ? 'text-warning' : 'text-success',
                 htmlspecialchars($cached_ip)
             );
         } elseif (!empty($fdata6)) {
             $cached_ipv6_s = explode('|', $fdata6);
             $cached_ipv6 = $cached_ipv6_s[0];
             echo sprintf(
-                '<font color="%s">%s</font>',
-                $ipv6addr != $cached_ipv6 ? 'red' : 'green',
+                '<font class="%s">%s</font>',
+                $ipv6addr != $cached_ipv6 ? 'text-warning' : 'text-success',
                 htmlspecialchars($cached_ipv6)
             );
         } else {
