afd4adf7f8bfa1e9cfaa2bc1ce83040b96019377
Nick Young <americanpilot84@gmail.com>
2019-03-28 09:42:42 -0500

    Allow wildcard for Google Domains.

--- dns/dyndns/src/www/services_dyndns_edit.php
+++ dns/dyndns/src/www/services_dyndns_edit.php
@@ -102,8 +102,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     do_input_validation($pconfig, $reqdfields, $reqdfieldsn, $input_errors);
 
     if (isset($pconfig['host']) && in_array('host', $reqdfields)) {
-        /* Namecheap can have a @. or *. in hostname */
-        if ($pconfig['type'] == "namecheap" && (substr($pconfig['host'], 0, 2) == '@.' || substr($pconfig['host'], 0, 2) == '*.')) {
+        /* Multiple vendors can have a @. or *. in hostname */
+        $wildcard_vendors = array(
+          "googledomains",
+          "namecheap"
+        );
+        if (in_array($pconfig['type'], $wildcard_vendors) && (substr($pconfig['host'], 0, 2) == '@.' || substr($pconfig['host'], 0, 2) == '*.')) {
             $host_to_check = substr($pconfig['host'], 2);
         } else {
             $host_to_check = $pconfig['host'];
