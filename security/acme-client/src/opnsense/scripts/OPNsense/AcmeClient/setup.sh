40f0212e61f6a417d7a19316c3f4c1f40fce03fe
Josh Wiles <josh.wiles@braums.com>
2019-04-04 09:59:49 -0500

    Fix ngnix permissions issue with acme-client
    This needs to be tested with HAProxy as well.

--- security/acme-client/src/opnsense/scripts/OPNsense/AcmeClient/setup.sh
+++ security/acme-client/src/opnsense/scripts/OPNsense/AcmeClient/setup.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
-ACME_DIRS="/var/etc/acme-client /var/etc/acme-client/certs /var/etc/acme-client/keys /var/etc/acme-client/configs /var/etc/acme-client/challenges /var/etc/acme-client/home"
+ACME_DIRS="/var/etc/acme-client /var/etc/acme-client/certs /var/etc/acme-client/keys /var/etc/acme-client/configs /var/etc/acme-client/home"
 
 for directory in ${ACME_DIRS}; do
     mkdir -p ${directory}
     chown -R root:wheel ${directory}
-    chmod -R 750 ${directory}
+    chmod -R 755 ${directory}
 done
+
+CHALLENGES_DIR="/var/etc/acme-client/challenges"
+
+mkdir -p ${CHALLENGES_DIR}
+chown -R www:www ${CHALLENGES_DIR}
+chmod -R 755 ${CHALLENGES_DIR}
 
 exit 0
