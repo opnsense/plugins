3d77503a6406c6bd655dfaf942d24947d84e13d3
Michael <m.muenz@gmail.com>
2019-05-17 07:33:57 +0200

    limit was reduced by bind
    domains with 230 chars already state an error

--- dns/bind/src/opnsense/scripts/OPNsense/Bind/dnsbl.sh
+++ dns/bind/src/opnsense/scripts/OPNsense/Bind/dnsbl.sh
@@ -213,7 +213,7 @@ zeusabuse() {
 install() {
 	# Put all files in correct format
 	for FILE in $(find ${WORKDIR} -type f); do
-		awk '{ if (length($1) < 245) print ""$1" CNAME .\n*."$1" CNAME ."}' ${FILE} | sort -u > ${FILE}.inc
+		awk '{ if (length($1) < 220) print ""$1" CNAME .\n*."$1" CNAME ."}' ${FILE} | sort -u > ${FILE}.inc
 	done
 	# Merge resulting files (/dev/null in case there are none)
 	cat $(find ${WORKDIR} -type f -name "*.inc") /dev/null | sort -u > ${DESTDIR}/dnsbl.inc
