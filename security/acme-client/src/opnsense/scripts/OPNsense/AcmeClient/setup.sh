3c5ae7dc9f183bf91ee9613a553adb7007d42af6
Jürgen Kellerer <juergen@k123.eu>
2019-08-13 22:38:38 +0200

    AcmeClient: Prevent mode 0750 on everything inside /var/etc/acme-client
    This change avoids that all contents in any subfolder is set to mode 0750 as this is illegal e.g. for private keys for SSH.

--- security/acme-client/src/opnsense/scripts/OPNsense/AcmeClient/setup.sh
+++ security/acme-client/src/opnsense/scripts/OPNsense/AcmeClient/setup.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
-ACME_DIRS="/var/etc/acme-client /var/etc/acme-client/certs /var/etc/acme-client/keys /var/etc/acme-client/configs /var/etc/acme-client/challenges /var/etc/acme-client/home"
+ACME_BASE="/var/etc/acme-client"
+ACME_DIRS="/var/etc/acme-client/certs /var/etc/acme-client/keys /var/etc/acme-client/configs /var/etc/acme-client/challenges /var/etc/acme-client/home"
 
+# Generating dirs if missing and setting owner and mode (recursively)
 for directory in ${ACME_DIRS}; do
     mkdir -p ${directory}
     chown -R root:wheel ${directory}
     chmod -R 750 ${directory}
 done
+
+# Setting owner and mode for base and immediate children (non recursive)
+chown root:wheel ${ACME_BASE} ${ACME_BASE}/*
+chmod 750 ${ACME_BASE} ${ACME_BASE}/*
 
 exit 0
