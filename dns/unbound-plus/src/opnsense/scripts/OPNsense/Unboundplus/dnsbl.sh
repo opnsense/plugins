32c4fea9745755c81d5a2e009cd2b64d0bd7219f
Michael <m.muenz@gmail.com>
2019-09-06 15:57:48 +0200

    Update dnsbl.sh

--- dns/unbound-plus/src/opnsense/scripts/OPNsense/Unboundplus/dnsbl.sh
+++ dns/unbound-plus/src/opnsense/scripts/OPNsense/Unboundplus/dnsbl.sh
@@ -218,16 +218,16 @@ simpletrack() {
 }
 
 install() {
-	# Put all files in correct format
-	for FILE in $(find ${WORKDIR} -type f); do
-		cat ${FILE} | sort -u > ${FILE}.inc
-		awk '{printf "server:\n", $1; printf "local-data: \"%s A 0.0.0.0\"\n", $1}' ${FILE} > ${FILE}.inc
-	done
-	# Merge resulting files (/dev/null in case there are none)
-	cat $(find ${WORKDIR} -type f -name "*.inc") /dev/null | sort -u > ${DESTDIR}/dnsbl.conf
-	chown unbound:unbound ${DESTDIR}/dnsbl.conf
-	rm -rf ${WORKDIR}
-	service unbound onereload
+        # Put all files in correct format
+        for FILE in $(find ${WORKDIR} -type f); do
+                cat ${FILE} | sort -u | awk '{printf "server:\n", $1; printf "local-data: \"%s A 0.0.0.0\"\n", $1}' > ${FILE}.inc
+        done
+        # Merge resulting files (/dev/null in case there are none)
+        cat $(find ${WORKDIR} -type f -name "*.inc") /dev/null > ${DESTDIR}/dnsbl.conf
+        chown unbound:unbound ${DESTDIR}/dnsbl.conf
+        rm -rf ${WORKDIR}
+        service unbound onerestart
+
 }
 
 DNSBL=${1}
