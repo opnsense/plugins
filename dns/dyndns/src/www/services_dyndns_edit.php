7592767233be98e89b2d38ad3d5abdbe6376bb12
Andreas Rupper <61623186+andreas-rupper@users.noreply.github.com>
2020-03-03 11:31:27 +0100

    Updated Cloudflare to use token auth

--- dns/dyndns/src/www/services_dyndns_edit.php
+++ dns/dyndns/src/www/services_dyndns_edit.php
@@ -75,7 +75,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     }
     $input_errors = array();
     $pconfig = $_POST;
-    if(($pconfig['type'] == "freedns" || $pconfig['type'] == "linode" || $pconfig['type'] == "linode-v6" || $pconfig['type'] == "namecheap") && $pconfig['username'] == "") {
+    if(($pconfig['type'] == "freedns" || $pconfig['type'] == "linode" || $pconfig['type'] == "linode-v6" || $pconfig['type'] == "namecheap" || $pconfig['type'] == "cloudflare-token" || $pconfig['type'] == "cloudflare-token-v6") && $pconfig['username'] == "") {
         $pconfig['username'] = "none";
     }
 
@@ -114,6 +114,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         switch ($pconfig['type']) {
             case 'cloudflare':
             case 'cloudflare-v6':
+			case 'cloudflare-token':
+			case 'cloudflare-token-v6':
             case 'eurodns':
             case 'googledomains':
             case 'linode':
@@ -153,7 +155,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         $dyndns['interface'] = $pconfig['interface'];
         $dyndns['zoneid'] = $pconfig['zoneid'];
         $dyndns['resourceid'] = $pconfig['resourceid'];
-        $dyndns['ttl'] = $pconfig['ttl'];
+		//This field *must* finally be an integer. So anything else has to be filtered...
+        $dyndns['ttl'] = intval(preg_replace('/[^0-9\.]/', '', $pconfig['ttl']));
         $dyndns['updateurl'] = $pconfig['updateurl'];
         // Trim hard-to-type but sometimes returned characters
         $dyndns['resultmatch'] = trim($pconfig['resultmatch'], "\t\n\r");
@@ -211,6 +214,15 @@ include("head.inc");
               case "azure":
               case "azurev6":
                 $(".type_azure").show();
+                break;
+              case 'cloudflare':
+              case 'cloudflare-v6':
+                $(".type_default").show();			  
+                $(".type_cloudflare").show();
+                break;
+              case 'cloudflare-token':
+              case 'cloudflare-token-v6':
+                $(".type_cloudflare").show();
                 break;
               default:
                 $(".type_default").show();
@@ -339,7 +351,7 @@ include("head.inc");
                       <?= gettext("Verify SSL peer") ?>
                     </td>
                   </tr>
-                  <tr>
+                  <tr class ="opt_field type_custom type_route53 type_azure type_default">
                     <td><a id="help_for_username" href="#" class="showhelp"><i class="fa fa-info-circle"></i></a> <?= gettext("Username") ?></td>
                     <td>
                       <input name="username" type="text" id="username" value="<?= $pconfig['username'] ?>" />
@@ -364,6 +376,7 @@ include("head.inc");
                         <br /><?= gettext('dynv6: Leave blank.') ?>
                         <br /><?= gettext('Azure: client secret of the AD application') ?>
                         <br /><?= gettext('Linode: Enter your Personal Access Token.') ?>
+						<br /><?= gettext('Cloudflare token: Enter your API token.') ?>
                       </div>
                     </td>
                   </tr>
@@ -413,12 +426,13 @@ include("head.inc");
                       </div>
                     </td>
                   </tr>
-                  <tr class="opt_field type_route53  type_azure">
+                  <tr class="opt_field type_route53 type_azure type_cloudflare">
                     <td><a id="help_for_ttl" href="#" class="showhelp"><i class="fa fa-info-circle"></i></a> <?=gettext("TTL");?></td>
                     <td>
                       <input name="ttl" type="text" id="ttl" value="<?= $pconfig['ttl'] ?>" />
                       <div class="hidden" data-for="help_for_ttl">
                         <?= gettext("Choose TTL for your dns record.") ?>
+						<br /><?= gettext('Cloudflare: value "1" means "Auto". Anything below 1 will be updated with value 1/Auto.') ?>
                       </div>
                     </td>
                   </tr>
