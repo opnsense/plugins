0d28cd36957c812239ff491a416cb60eab23ee24
Franco Fichtner <franco@opnsense.org>
2020-03-04 09:19:39 +0100

    devel/helloworld: sync with master

--- devel/helloworld/src/opnsense/mvc/app/controllers/OPNsense/HelloWorld/Api/ServiceController.php
+++ devel/helloworld/src/opnsense/mvc/app/controllers/OPNsense/HelloWorld/Api/ServiceController.php
@@ -30,8 +30,8 @@
 
 namespace OPNsense\HelloWorld\Api;
 
-use \OPNsense\Base\ApiControllerBase;
-use \OPNsense\Core\Backend;
+use OPNsense\Base\ApiControllerBase;
+use OPNsense\Core\Backend;
 
 /**
  * Class ServiceController
