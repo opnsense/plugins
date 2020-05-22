3d4416cf260d40112afca1996db139932342ec5a
Ad Schellevis <AdSchellevis@users.noreply.github.com>
2020-05-22 13:12:28 +0200

    Stunnel: add identd (#1845)
    stunnel: add identd service and plumbing

--- security/stunnel/src/opnsense/service/templates/OPNsense/Stunnel/rc.conf.d
+++ security/stunnel/src/opnsense/service/templates/OPNsense/Stunnel/rc.conf.d
@@ -3,6 +3,7 @@ stunnel_enable="YES"
 stunnel_pidfile="/var/run/stunnel/stunnel.pid"
 
 mkdir -p /var/run/stunnel/certs
+mkdir -p /var/run/stunnel/logs
 chown -R stunnel:stunnel /var/run/stunnel
 chmod -R 700 /var/run/stunnel
 
