e16d1dc8adc36a354e4897d2c376555f7a534ce6
botboe <5389669+botboe@users.noreply.github.com>
2020-06-24 09:06:39 +0000

    Changes according to pull-request's comments

--- net/radsecproxy/src/opnsense/mvc/app/controllers/OPNsense/RadSecProxy/Api/ServiceController.php
+++ net/radsecproxy/src/opnsense/mvc/app/controllers/OPNsense/RadSecProxy/Api/ServiceController.php
@@ -31,24 +31,10 @@ class ServiceController extends ApiMutableServiceControllerBase
 {
     protected static $internalServiceClass = '\OPNsense\RadSecProxy\RadSecProxy';
     protected static $internalServiceTemplate = 'OPNsense/RadSecProxy';
-    protected static $internalServiceEnabled = 'general.Enabled';
+    protected static $internalServiceEnabled = 'general.enabled';
     protected static $internalServiceName = 'radsecproxy';
     protected function reconfigureForceRestart()
     {
         return 0;
-    }
-
-    public function checkconfigAction()
-    {
-        $backend = new Backend();
-        // first generate template based on current configuration
-        $backend->configdRun('template reload OPNsense/RadSecProxy');
-        
-        // now export all the required files (or syntax check will fail)
-        $backend->configdRun("radsecproxy setup");
-
-        // finally run the syntax check
-        $response = $backend->configdRun("radsecproxy checkconfig");
-        return array("result" => trim($response));
     }
 }
