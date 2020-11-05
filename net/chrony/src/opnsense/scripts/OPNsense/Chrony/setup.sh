e0c09fa3570858bfd02bf391fb15f2ff0b8883b1
Michael <m.muenz@gmail.com>
2020-11-05 09:26:24 +0100

    net/chrony: add NTS peer mode (#2087)

--- net/chrony/src/opnsense/scripts/OPNsense/Chrony/setup.sh
+++ net/chrony/src/opnsense/scripts/OPNsense/Chrony/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-mkdir -p /var/db/chrony/ /var/run/chrony/
-chown -R chronyd:chronyd /var/db/chrony/ /var/run/chrony/
-chmod 750 /var/db/chrony/ /var/run/chrony/
+mkdir -p /var/db/chrony /var/lib/chrony /var/run/chrony
+chown -R chronyd:chronyd /var/db/chrony /var/lib/chrony /var/run/chrony
+chmod 750 /var/db/chrony /var/lib/chrony /var/run/chrony
