e2ab8c05f87ebc61e200bbe1c7d4cb4c4f4cd86b
Franco Fichtner <franco@opnsense.org>
2021-02-01 14:44:59 +0100

    Framework: exclude .md files
    (cherry picked from commit 850e1c11a8d1ac10948b14ec961ce8b994e84c30)

--- Mk/plugins.mk
+++ Mk/plugins.mk
@@ -294,7 +294,7 @@ lint-php: check
 	@find ${.CURDIR}/src \
 	    ! -name "*.xml" ! -name "*.xml.sample" ! -name "*.eot" \
 	    ! -name "*.svg" ! -name "*.woff" ! -name "*.woff2" \
-	    ! -name "*.otf" ! -name "*.png" ! -name "*.js" \
+	    ! -name "*.otf" ! -name "*.png" ! -name "*.js" ! -name "*.md" \
 	    ! -name "*.scss" ! -name "*.py" ! -name "*.ttf" \
 	    ! -name "*.tgz" ! -name "*.xml.dist" ! -name "*.sh" \
 	    -type f -print0 | xargs -0 -n1 php -l
