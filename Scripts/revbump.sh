3e02a4c67c78c85b7583947841cee04583df6496
Franco Fichtner <franco@opnsense.org>
2021-02-15 10:10:07 +0100

    Framework: tedious work on FreeBSD...

--- Scripts/revbump.sh
+++ Scripts/revbump.sh
@@ -12,5 +12,6 @@ REV=$(make -C ${DIR} -V PLUGIN_REVISION)
 REV=$(expr ${REV} \+ 1)
 
 grep -v ^PLUGIN_REVISION ${DIR}/Makefile > ${DIR}/Makefile.tmp
-sed -e "s/^\(PLUGIN_VERSION.*\)/\1\nPLUGIN_REVISION=	${REV}/g" ${DIR}/Makefile.tmp > ${DIR}/Makefile
+sed -e "s/^\(PLUGIN_VERSION.*\)/\1%PLUGIN_REVISION=	${REV}/g" \
+    ${DIR}/Makefile.tmp | tr '%' '\n' > ${DIR}/Makefile
 rm -f ${DIR}/Makefile.tmp
