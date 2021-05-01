cb4fce537bda25d226581237e36d27a0b76c0af4
agh1467 <agh1467@rcpt.at>
2021-05-01 14:06:43 +0000

    dns/dnscrypt-proxy: get-relays.py read config instead of through php
    * Integrated parse the config into python instead of using
      php to do it and then passing to python.
    * Added a configuration file with some globals to allow code
      to be more portable.
    * Remove unused php file.
    * Updated configd configuration to use only the python script.
    * Updated comments there as well.
    * Other general comments.

--- dns/dnscrypt-proxy/src/opnsense/scripts/OPNsense/Dnscryptproxy/get-relays.py
+++ dns/dnscrypt-proxy/src/opnsense/scripts/OPNsense/Dnscryptproxy/get-relays.py
@@ -1,13 +1,65 @@
 #!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+    Copyright (c) 2014-2019 Ad Schellevis <ad@opnsense.org>
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+     this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
+
+    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    POSSIBILITY OF SUCH DAMAGE.
+
+    --------------------------------------------------------------------------------------
+
+    package : Dnscryptproxy
+    function: returns a list or dict of the currently available relays from the enabled sources files.
+"""
 
 import os, sys
 import json
 import base64
 import struct
+import configparser
+import xml.etree.ElementTree
+
+# Parse the dnscrypt-proxy config for some settings.
+cnf = configparser.ConfigParser()
+cnf.read('/usr/local/opnsense/service/conf/dnscrypt-proxy.conf')
+
+# Create a dictionary for the key:value pairs.
+plugin = dict()
+for envKey in cnf.items('plugin'):
+    plugin[envKey[0]] = envKey[1]
 
 args = sys.argv
 
+def error_out(message):
+    result['error'] = message
+    result['status'] = "failed"
+    print(json.dumps(result, indent=4))
+    sys.exit()
+
+
 def main():
+    global result
+    result = {}
 
     # The only way to tell if any given resolver is actually a relay is to
     # analyze the stamp. So we go through all of sources files, and pick out
@@ -15,19 +67,39 @@ def main():
 
     # Needs some error handling, probably should always return a json?
 
-    dnscrypt_proxy_path = "/usr/local/etc/dnscrypt-proxy"
+    dnscrypt_proxy_path = plugin['config_dir']
 
-    if len(args) > 1:
-        for cache_file in args[1].split(","):
+    if len(args) > 0:
+
+        # Parse the config.xml
+        tree = xml.etree.ElementTree.parse('/conf/config.xml')
+        rootNode = tree.getroot()
+
+        # Set up our list, and walk the tree to our desired node.
+        cache_files = []
+        config_path = "./OPNsense/dnscrypt-proxy/sources/source"
+        for node in rootNode.findall(config_path):
+            for childnode in node.iter():
+                # For each source, if enabled, get the cache_file of that node, and append to our list.
+                if childnode.tag == "enabled":
+                    if childnode.text == "1":
+                        cache_files.append(node.find("cache_file").text)
+
+        # Iterate through our cache files, and parse for relays.
+        for cache_file in cache_files:
             file = dnscrypt_proxy_path + "/" + cache_file
 
             if not os.path.isfile(file):
-                print("File path {} does not exist. Exiting...".format(file))
-                sys.exit()
+                error_out("File path {} does not exist. Exiting...".format(file))
 
-            if len(args) == 3:
-                if args[2] == "names":
+            # Set the type of object we need depending on the type of output we would like.
+            # dict is used for dropdowns
+            # list is used for bootgrid
+            if len(args) == 2:
+                if args[1] == "names":
                     relays = {}
+                else:
+                    error_out("Invalid mode specified: {}".format(args[1]))
             else:
                 relays = []
 
@@ -57,16 +129,20 @@ def main():
                             protocol = struct.unpack('<B', stamp[:1])[0]
                         except Exception as e:
                             raise Exception('Unable to consume protocol', e)
-                        if protocol == 129: # This is the protocol number for relays.
-                            if len(args) == 3:
-                                if args[2] == "names":
+                        # The protocol number for relays is 129.
+                        if protocol == 129:
+                            # If we found a relay add it to the appropriate object.
+                            if len(args) == 2:
+                                if args[1] == "names":
                                     relays.update({resolver_name:resolver_name})
                             else:
                                 relays.append({"name":resolver_name,"description":description.strip()})
+                        # Reset these vars since we're done entry.
                         desc_bit = False
                         description = ""
         # Output whatever relays we found to stdout.
         print (json.dumps(relays, indent=4))
+
 
 if __name__ == '__main__':
     main()
