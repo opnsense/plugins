b2a556f05718defea7dcece39eb7847083d15b38
jan-win1993 <47572976+jan-win1993@users.noreply.github.com>
2021-05-19 12:32:51 +0200

    Add puppet-agent plugin (#2358)

---
+++ sysutils/puppet-agent/src/opnsense/mvc/app/controllers/OPNsense/PuppetAgent/Api/SettingsController.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ *    Copyright (C) 2021 Jan Winkler
+ *    Copyright (C) 2015-2019 Deciso B.V.
+ *
+ *    All rights reserved.
+ *
+ *    Redistribution and use in source and binary forms, with or without
+ *    modification, are permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ *    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ *    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ *    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *    POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+namespace OPNsense\PuppetAgent\Api;
+
+use OPNsense\Base\ApiControllerBase;
+use OPNsense\PuppetAgent\PuppetAgent;
+use OPNsense\Core\Config;
+
+/**
+ * Class SettingsController Handles settings related API actions for the PuppetAgent module
+ * @package OPNsense\PuppetAgent
+ */
+class SettingsController extends ApiControllerBase
+{
+    /**
+     * retrieve PuppetAgent general settings
+     * @return array general settings
+     * @throws \OPNsense\Base\ModelException
+     * @throws \ReflectionException
+     */
+    public function getAction()
+    {
+        // define list of configurable settings
+        $result = array();
+        if ($this->request->isGet()) {
+            $mdlPuppetAgent = new PuppetAgent();
+            $result['puppetagent'] = $mdlPuppetAgent->getNodes();
+        }
+        return $result;
+    }
+
+    /**
+     * update PupppetAgent settings
+     * @return array status
+     * @throws \OPNsense\Base\ModelException
+     * @throws \ReflectionException
+     */
+    public function setAction()
+    {
+        $result = array("result" => "failed");
+        if ($this->request->isPost()) {
+            // load model and update with provided data
+            $mdlPuppetAgent = new PuppetAgent();
+            $mdlPuppetAgent->setNodes($this->request->getPost("puppetagent"));
+
+            // perform validation
+            $valMsgs = $mdlPuppetAgent->performValidation();
+            foreach ($valMsgs as $field => $msg) {
+                if (!array_key_exists("validations", $result)) {
+                    $result["validations"] = array();
+                }
+                $result["validations"]["puppetagent." . $msg->getField()] = $msg->getMessage();
+            }
+
+            // serialize model to config and save
+            if ($valMsgs->count() == 0) {
+                $mdlPuppetAgent->serializeToConfig();
+                Config::getInstance()->save();
+                $result["result"] = "saved";
+            }
+        }
+        return $result;
+    }
+}
