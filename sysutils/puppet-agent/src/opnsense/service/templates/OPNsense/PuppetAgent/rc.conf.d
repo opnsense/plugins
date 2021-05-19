64b474a5c3b173e0d393432477dd7072bf637a9f
Frank Wall <fw@moov.de>
2021-05-19 12:13:11 +0200

    streamline rc.conf variables
    Co-authored-by: Franco Fichtner <franco@lastsummer.de>

--- sysutils/puppet-agent/src/opnsense/service/templates/OPNsense/PuppetAgent/rc.conf.d
+++ sysutils/puppet-agent/src/opnsense/service/templates/OPNsense/PuppetAgent/rc.conf.d
@@ -1,5 +1,5 @@
 {% if helpers.exists('OPNsense.puppetagent.general') and OPNsense.puppetagent.general.Enabled|default("0") == "1" %}
 puppet_enable="YES"
 {% else %}
-puppet_enable="No"
+puppet_enable="NO"
 {% endif %}
