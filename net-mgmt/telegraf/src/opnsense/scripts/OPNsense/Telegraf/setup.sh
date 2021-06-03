05d395947b0ea3d4faa31dd84233c8517c637799
Franco Fichtner <franco@lastsummer.de>
2021-06-03 13:15:57 +0200

    Update net-mgmt/telegraf/src/opnsense/scripts/OPNsense/Telegraf/setup.sh

--- net-mgmt/telegraf/src/opnsense/scripts/OPNsense/Telegraf/setup.sh
+++ net-mgmt/telegraf/src/opnsense/scripts/OPNsense/Telegraf/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p /usr/local/etc/telegraf.d /var/log/telegraf
-chown -R telegraf:telegraf /usr/local/etc/telegraf.d /var/log/telegraf 
+chown -R telegraf:telegraf /usr/local/etc/telegraf.d /var/log/telegraf
 chmod 750 /usr/local/etc/telegraf.d /var/log/telegraf
 
 /usr/sbin/pw groupmod proxy -m telegraf
