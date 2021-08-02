b8dcfba9fa24337e9fea1904b0ae5119504c3784
Michael <m.muenz@gmail.com>
2021-08-02 09:05:09 +0200

    dns/dnscrypt-proxy: fix logging (#2467)
    Fix logging due to Phalcon4 update

--- dns/dnscrypt-proxy/src/opnsense/scripts/OPNsense/Dnscryptproxy/setup.sh
+++ dns/dnscrypt-proxy/src/opnsense/scripts/OPNsense/Dnscryptproxy/setup.sh
@@ -2,3 +2,5 @@
 
 mkdir -p /var/log/dnscrypt-proxy
 chown -R _dnscrypt-proxy:_dnscrypt-proxy /var/log/dnscrypt-proxy
+(cd /var/log && ln -s dnscrypt-proxy dnscryptproxy)
+chown -R _dnscrypt-proxy:_dnscrypt-proxy /var/log/dnscryptproxy
