ffa7ad54bc778a6cd804248a89f2740e92890135
Adrian Fedoreanu <phedoreanu@users.noreply.github.com>
2021-11-04 07:57:50 +0000

    dns/dyndns: add support for 1984Hosting (#1952)

--- dns/dyndns/src/www/services_dyndns_edit.php
+++ dns/dyndns/src/www/services_dyndns_edit.php
@@ -75,7 +75,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     }
     $input_errors = array();
     $pconfig = $_POST;
-    if(($pconfig['type'] == "freedns" || $pconfig['type'] == "linode" || $pconfig['type'] == "linode-v6" || $pconfig['type'] == "namecheap" || $pconfig['type'] == "cloudflare-token" || $pconfig['type'] == "cloudflare-token-v6" || $pconfig['type'] == "desec" || $pconfig['type'] == "desec-v4-v6" || $pconfig['type'] == "desec-v6") && $pconfig['username'] == "") {
+
+    if(($pconfig['type'] == "freedns" || $pconfig['type'] == "linode" || $pconfig['type'] == "linode-v6" || $pconfig['type'] == "namecheap" || $pconfig['type'] == "cloudflare-token" || $pconfig['type'] == "cloudflare-token-v6" || $pconfig['type'] == "desec" || $pconfig['type'] == "desec-v4-v6" || $pconfig['type'] == "desec-v6" || $pconfig['type'] == "1984-hosting") && $pconfig['username'] == "") {
         $pconfig['username'] = "none";
     }
 
@@ -128,6 +129,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
             case 'linode':
             case 'linode-v6':
             case 'namecheap':
+            case '1984-hosting':
                 $host_to_check = preg_replace('/^[@*]\./', '', $host_to_check);
                 break;
             default:
