5850480b07c804e31866891731a31fc7beaa1e93
Alexander Noack <alex@keeeb.com>
2021-11-30 19:55:44 +0100

    Changed copyright

--- security/acme-client/src/opnsense/mvc/app/library/OPNsense/AcmeClient/LeAutomation/AcmeUnifi.php
+++ security/acme-client/src/opnsense/mvc/app/library/OPNsense/AcmeClient/LeAutomation/AcmeUnifi.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (C) 2021 Frank Wall
+ * Copyright (C) 2021 Alexander Noack
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
