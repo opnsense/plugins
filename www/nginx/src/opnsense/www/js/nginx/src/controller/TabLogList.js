12738579f18b308521dc067c0199450f0f728b49
Manuel Faux <mfaux@conf.at>
2021-12-16 00:35:41 +0100

    www/nginx: Load logs for single server on demand
    Loading of the logging page takes very look if many servers are configured.
    With this change the single log files for a server are loaded only after
    clicking the down error for the server entry in the log view.

--- www/nginx/src/opnsense/www/js/nginx/src/controller/TabLogList.js
+++ www/nginx/src/opnsense/www/js/nginx/src/controller/TabLogList.js
@@ -5,13 +5,15 @@ let TabLogList = Backbone.View.extend({
     tagName: 'li',
     events: {
         "click .mainentry": "mainMenuClick",
-        "click .menuEntry": "menuEntryClick"
+        "click .menuEntry": "menuEntryClick",
+        "click .dropdown-toggle": "menuDropdownClick"
     },
 
     initialize: function(data) {
         this.listenTo(this.collection, "update", this.render);
         this.logType = data.logType;
         this.logview = data.logview;
+        this.render();
     },
 
     render: function() {
@@ -40,6 +42,9 @@ let TabLogList = Backbone.View.extend({
             $(`#tab_${this.model.get('id')} li`).removeClass('active');
             $(`#subtab_item_${this.model.get('id')}_${this.collection.models[0].get('number')}`).parent().addClass('active');
         }
+    },
+    menuDropdownClick: function (event) {
+        this.collection.fetch();
     },
     menuEntryClick: function (event) {
         this.handleElementClick(event.target.dataset['modelUuid'], event.target.dataset['modelFileno']);
