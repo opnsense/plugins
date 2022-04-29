773edbde26586b163cbb03b8c25b5affbc1a70ec
clanto007 <44651109+clanto007@users.noreply.github.com>
2022-04-29 12:36:15 +0200

    Delete setup_zabbix_proxy.sh

--- net-mgmt/net-snmp/src/opnsense/scripts/OPNsense/Netsnmp/setup_zabbix_proxy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mkdir /usr/local/etc/snmp
-echo 'authCommunity log,execute,net public' >> /usr/local/etc/snmp/snmptrapd.conf
-echo 'disableAuthorization yes' >> /usr/local/etc/snmp/snmptrapd.conf
-echo 'perl do "/usr/bin/zabbix_trap_receiver.pl";' >> /usr/local/etc/snmp/snmptrapd.conf
-wget https://git.zabbix.com/projects/ZBX/repos/zabbix/raw/misc/snmptrap/zabbix_trap_receiver.pl -O /usr/bin/zabbix_trap_receiver.pl
