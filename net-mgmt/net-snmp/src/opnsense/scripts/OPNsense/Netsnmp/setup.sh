221eb1d884dea915e602494bd3f8d9b34c14c718
clanto007 <44651109+clanto007@users.noreply.github.com>
2022-04-29 12:46:24 +0200

    Update setup.sh

--- net-mgmt/net-snmp/src/opnsense/scripts/OPNsense/Netsnmp/setup.sh
+++ net-mgmt/net-snmp/src/opnsense/scripts/OPNsense/Netsnmp/setup.sh
@@ -7,9 +7,3 @@ chmod 755 /var/net-snmp
 mkdir -p /var/net-snmp/mib_indexes
 chown -R root:wheel /var/mib_indexes
 chmod 700 /var/net-snmp/mib_indexes
-
-mkdir /usr/local/etc/snmp
-echo 'authCommunity log,execute,net public' >> /usr/local/etc/snmp/snmptrapd.conf
-echo 'disableAuthorization yes' >> /usr/local/etc/snmp/snmptrapd.conf
-echo 'perl do "/usr/bin/zabbix_trap_receiver.pl";' >> /usr/local/etc/snmp/snmptrapd.conf
-wget https://git.zabbix.com/projects/ZBX/repos/zabbix/raw/misc/snmptrap/zabbix_trap_receiver.pl -O /usr/bin/zabbix_trap_receiver.pl
