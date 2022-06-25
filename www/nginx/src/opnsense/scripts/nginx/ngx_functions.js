16d585db78b31cde45d65d3805fd6f7104d5e3d3
kulikov-a <36099472+kulikov-a@users.noreply.github.com>
2022-06-25 22:13:39 +0300

    rfc8701

--- www/nginx/src/opnsense/scripts/nginx/ngx_functions.js
+++ www/nginx/src/opnsense/scripts/nginx/ngx_functions.js
@@ -1,13 +1,15 @@
 var fs = require('fs');
 var tls_fingerprints = JSON.parse(fs.readFileSync('/usr/local/etc/nginx/tls_fingerprints.json'));
+// ignore GREASE cipher suite values when compiling a browser fingerprint (see rfc8701)
+const GREASE = ["0x0a0a", "0x1a1a", "0x2a2a", "0x3a3a", "0x4a4a", "0x5a5a", "0x6a6a", "0x7a7a", "0x8a8a", "0x9a9a", "0xaaaa", "0xbaba", "0xcaca", "0xdada", "0xeaea", "0xfafa"];
 
 function check_cipher_array(r, browser_ciphers, fingerprint_ciphers, result) {
-  if (result.status == 'Intercepted') {
+  if (result.status.indexOf('Intercepted') !== -1) {
     return;
   }
   if (browser_ciphers.length > fingerprint_ciphers.length) {
-    // the proxy supports more cipers than the browser -> intercepted
-    result.status = "Intercepted";
+    // the proxy supports more ciphers than the browser -> intercepted
+    result.status = "Intercepted(excess suite)";
     return;
   }
   var browser_cipher;
@@ -18,9 +20,9 @@ function check_cipher_array(r, browser_ciphers, fingerprint_ciphers, result) {
     browser_cipher = browser_ciphers[browser_cipher_index];
     current_index = fingerprint_ciphers.indexOf(browser_cipher);
     if (current_index === -1 || current_index <= last_index) {
-      // a cipher has been found, which is not supported by the browser
+      // a cipher has been found, which is not supported by the browser or order of preference changed
       // such a connection is definitly intercepted
-      result.status = "Intercepted";
+      result.status = "Intercepted(excess suite or wrong order)";
       return;
     }
     last_index = current_index;
@@ -36,11 +38,23 @@ function check_intercept(r) {
       var ua = r.headersIn['User-Agent'];
       if (ua in tls_fingerprints) {
         var fp = tls_fingerprints[ua];
+        fp.ciphers = fp.ciphers.filter( function( el ) {
+          return GREASE.indexOf( el ) < 0;
+        } );
+        fp.curves = fp.curves.filter( function( el ) {
+          return GREASE.indexOf( el ) < 0;
+        } );
         var browser_ciphers = r.variables.ssl_ciphers.split(':');
+        browser_ciphers = browser_ciphers.filter( function( el ) {
+          return GREASE.indexOf( el ) < 0;
+        } );
         check_cipher_array(r, browser_ciphers, fp.ciphers, tls_result);
         if (r.variables.ssl_curves != '')
         {
           var browser_curves = r.variables.ssl_curves.split(':');
+          browser_curves = browser_curves.filter( function( el ) {
+            return GREASE.indexOf( el ) < 0;
+          } );
           check_cipher_array(r, browser_curves, fp.curves, tls_result);
         }
       }
