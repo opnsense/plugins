91a12520e94d335448e880f0defd5af0eaf60551
Franco Fichtner <franco@opnsense.org>
2022-07-04 11:25:53 +0200

    dns/ddclient: sync with master

--- dns/ddclient/src/opnsense/service/templates/OPNsense/ddclient/rc.conf.d
+++ dns/ddclient/src/opnsense/service/templates/OPNsense/ddclient/rc.conf.d
@@ -1,5 +1,6 @@
 {% if not helpers.empty('OPNsense.DynDNS.general.enabled') %}
 ddclient_enable="YES"
+ddclient_flags="-daemon {{OPNsense.DynDNS.general.daemon_delay|default('300')}}"
 {% else %}
 ddclient_enable="NO"
 {% endif %}
