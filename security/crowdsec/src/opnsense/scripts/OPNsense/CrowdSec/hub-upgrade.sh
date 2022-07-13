a427756cf2092a30d67d9d843a45bc8420848088
mmetc <92726601+mmetc@users.noreply.github.com>
2022-07-13 15:20:24 +0200

    security/crowdsec: v1.0 (#3030)

--- security/crowdsec/src/opnsense/scripts/OPNsense/CrowdSec/hub-upgrade.sh
+++ security/crowdsec/src/opnsense/scripts/OPNsense/CrowdSec/hub-upgrade.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
+/usr/local/bin/cscli --error hub update \
+    && /usr/local/bin/cscli --error hub upgrade
+
 if [ ! -e "/usr/local/etc/crowdsec/collections/opnsense.yaml" ]; then
     /usr/local/bin/cscli --error collections install crowdsecurity/opnsense
 fi
 
-/usr/local/bin/cscli --error hub update \
-    && /usr/local/bin/cscli --error hub upgrade
-
 if service crowdsec enabled; then
-    # have to check status explicitly because "restart" can set $? = 0 even when failing
     if ! service crowdsec status >/dev/null 2>&1; then
         service crowdsec start >/dev/null 2>&1 || :
     else
-        service crowdsec restart >/dev/null 2>&1 || :
+        service crowdsec reload >/dev/null 2>&1 || :
     fi
 fi
