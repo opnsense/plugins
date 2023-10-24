e474d4b17b1de9640d605be5a10aa17ab273c7f5
Franco Fichtner <franco@opnsense.org>
2023-10-24 12:37:57 +0200

    net/radsecproxy: style scrubbing

--- net/radsecproxy/src/opnsense/service/templates/OPNsense/RadSecProxy/rc.conf.d
+++ net/radsecproxy/src/opnsense/service/templates/OPNsense/RadSecProxy/rc.conf.d
@@ -1,8 +1,8 @@
 {% if not helpers.empty('OPNsense.radsecproxy.general.enabled') %}
 radsecproxy_enable="YES"
-{% else %}
-radsecproxy_enable="NO"
-{% endif %}
 radsecproxy_user="root"
 radsecproxy_group="wheel"
 radsecproxy_setup="/usr/local/opnsense/scripts/OPNsense/RadSecProxy/setup.sh"
+{% else %}
+radsecproxy_enable="NO"
+{% endif %}
