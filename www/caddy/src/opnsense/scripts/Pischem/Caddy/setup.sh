2142360cd3a7b5defaf933c67dbf992165d1100f
Monviech <gitacc@pischem.com>
2024-02-26 11:14:57 +0000

    Cleanup in setup.sh script. Remove LICENSE file. Changed caddy pidfile location in plugin.inc since the path changed when using the standard rc.d service script.

--- www/caddy/src/opnsense/scripts/Pischem/Caddy/setup.sh
+++ www/caddy/src/opnsense/scripts/Pischem/Caddy/setup.sh
@@ -8,7 +8,6 @@ CADDY_OCSP_DIR="${CADDY_DIR}/ocsp"
 CADDY_LOCKS_DIR="${CADDY_DIR}/locks"
 CADDY_LOG_DIR="/var/log/caddy/access"
 CADDY_CONF_DIR="${CADDY_DIR}/caddy.d"
-CADDY_CONFIG_DIR="${CADDY_DIR}/.config/caddy" # Additional config directory
 
 # Create Caddy configuration directories with appropriate permissions
 mkdir -p "${CADDY_DIR}"
@@ -17,7 +16,6 @@ mkdir -p "${CADDY_CERTS_DIR}"
 mkdir -p "${CADDY_OCSP_DIR}"
 mkdir -p "${CADDY_LOCKS_DIR}"
 mkdir -p "${CADDY_CONF_DIR}"
-mkdir -p "${CADDY_CONFIG_DIR}"
 
 # Set permissions for Caddy configuration directories
 chown -R root:wheel "${CADDY_DIR}"
@@ -30,16 +28,11 @@ mkdir -p "${CADDY_LOG_DIR}"
 chown -R root:wheel "${CADDY_LOG_DIR}"
 chmod -R 750 "${CADDY_LOG_DIR}"
 
-# Ensure the Caddy service script is executable
-chmod +x /usr/local/etc/rc.d/caddy
-
-# Ensure the Caddy binary is executable
-chmod +x /usr/local/bin/caddy
-
 # Format and overwrite the Caddyfile
-cd "${CADDY_DIR}" && /usr/local/bin/caddy fmt --overwrite
+(cd "${CADDY_DIR}" && /usr/local/bin/caddy fmt --overwrite)
 
 # Write custom certs from the OPNsense Trust Store into a directory where Caddy can read them
 /usr/local/opnsense/scripts/Pischem/Caddy/caddy_certs.php
 
+# Optional Debug message
 # echo "Caddy installation completed. All caddy directories and files created successfully."
