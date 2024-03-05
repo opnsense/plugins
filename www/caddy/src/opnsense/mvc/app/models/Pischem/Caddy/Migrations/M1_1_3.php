f3db69d5d96c95f012358b4368a9749057837c77
Monviech <79600909+Monviech@users.noreply.github.com>
2024-03-05 20:21:13 +0100

    Update M1_1_3.php
    Change the Migration script to read the "Description" key and value from $config, but write back to the $model "description" key and value.

--- www/caddy/src/opnsense/mvc/app/models/Pischem/Caddy/Migrations/M1_1_3.php
+++ www/caddy/src/opnsense/mvc/app/models/Pischem/Caddy/Migrations/M1_1_3.php
@@ -1,45 +1,87 @@
 <?php
 
+/*
+ *    Copyright (C) 2024 Cedrik Pischem
+ *    All rights reserved.
+ *
+ *    Redistribution and use in source and binary forms, with or without
+ *    modification, are permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ *    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ *    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ *    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *    POSSIBILITY OF SUCH DAMAGE.
+ */
+
 namespace Pischem\Caddy\Migrations;
 
-use OPNsense\Core\Config;
 use OPNsense\Base\BaseModelMigration;
+use OPNsense\Core\Config;
 
 class M1_1_3 extends BaseModelMigration
 {
     public function run($model)
     {
-        // Read the current configuration settings from the system
-        $cfgObj = Config::getInstance()->object();
-
-        // Flag to keep track of whether we've made any changes that need saving
+        // Load the current system configuration
+        $config = Config::getInstance()->object();
+        
+        // Track whether any changes are made during the migration
         $hasChanges = false;
 
-        // Checking if there's a 'reverseproxy' section under 'Pischem->caddy' in the config
-        if (isset($cfgObj->Pischem->caddy->reverseproxy)) {
+        // Ensure there are reverse proxy configurations to process
+        if (!empty($config->Pischem->caddy->reverseproxy)) {
+        
+            // Loop through each reverse proxy configuration in the stored configuration config.xml
+            foreach ($config->Pischem->caddy->reverseproxy->children() as $configNode) {
+            
+                // Extract the UUID attribute to identify the configuration item
+                $uuid = (string)$configNode->attributes()->uuid;
+                
+                // Check if the current configuration item has a 'Description' to migrate
+                if (!empty($configNode->Description)) {
+                
+                    // Store the value of 'Description' for migration.
+                    $descriptionValue = (string)$configNode->Description;
 
-            // Iterating over each item in the 'reverseproxy' section.
-            foreach ($cfgObj->Pischem->caddy->reverseproxy->children() as $item) {
+                    // Attempt to locate the corresponding node in the model using the UUID
+                    $modelNode = null;
+                    
+                    // Retrieve reverse proxy items from the model for matching UUID
+                    $reverseProxies = $model->getNodeByReference('reverseproxy')->iterateItems();
+                    foreach ($reverseProxies as $item) {
+                        foreach ($item->iterateItems() as $modelUuid => $node) {
+                            if ($uuid === $modelUuid) {
+                                $modelNode = $node;
+                                break 2; // Break from both loops once the node is found
+                            }
+                        }
+                    }
 
-                // If an item has the old 'Description' tag, we need to update it
-                if (isset($item->Description)) {
-
-                    // Capturing the old description's value before we remove the tag
-                    $descriptionValue = (string)$item->Description;
-
-                    // Removing the old 'Description' tag and creating a new 'description' tag with the same value
-                    unset($item->Description);
-                    $item->addChild('description', $descriptionValue);
-
-                    // Track a change that needs to be saved later
-                    $hasChanges = true;
+                    // If a matching node is found in the model, migrate the 'Description' value to 'description' value
+                    if ($modelNode !== null) {
+                        $modelNode->description = $descriptionValue;
+                        $hasChanges = true;
+                    }
                 }
             }
+        }
 
-            // After going through all items, if any changes were made, we save the updated configuration
-            if ($hasChanges) {
-                Config::getInstance()->save();
-            }
+        // Save model if changes have been made
+        if ($hasChanges) {
+            parent::run($model);
         }
     }
 }
