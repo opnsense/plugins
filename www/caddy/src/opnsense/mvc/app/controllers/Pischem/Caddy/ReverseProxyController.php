a04b27666922dd89697e28186aca33d027e79284
Monviech <79600909+Monviech@users.noreply.github.com>
2024-03-06 16:03:06 +0100

    Update ReverseProxyController.php

--- www/caddy/src/opnsense/mvc/app/controllers/Pischem/Caddy/ReverseProxyController.php
+++ www/caddy/src/opnsense/mvc/app/controllers/Pischem/Caddy/ReverseProxyController.php
@@ -29,13 +29,13 @@
  *
  */
 
-namespace Pischem\Caddy;
+namespace OPNsense\Caddy;
 
 use OPNsense\Base\IndexController;
 
 class ReverseProxyController extends IndexController {
     public function indexAction() {
-        $this->view->pick('Pischem/Caddy/reverse_proxy');
+        $this->view->pick('OPNsense/Caddy/reverse_proxy');
         $this->view->formDialogReverseProxy = $this->getForm("dialogReverseProxy");
         $this->view->formDialogSubdomain = $this->getForm("dialogSubdomain");
         $this->view->formDialogHandle = $this->getForm("dialogHandle");
