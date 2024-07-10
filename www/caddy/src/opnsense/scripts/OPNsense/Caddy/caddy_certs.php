d777a605920921249ec120c8679f0e50215a0266
Monviech <79600909+Monviech@users.noreply.github.com>
2024-07-10 11:38:46 +0200

    www/caddy: Cleanup model, php (PSR-12), python (PEP 8) and jinja2 files (#4060)
    * www/caddy: Remove unsused code in validation.
    
    * www/caddy: Cleanup Caddyfile template for better readability with improved indentation.
    
    * www/caddy: Cleanup rc.conf.d/caddy
    
    * www/caddy: Cleanup model.
    
    * www/caddy: Cleanup Caddyfile template some more.
    
    * www/caddy: Roll back changes to Caddyfile structure, don't fix what ain't broken.
    
    * www/caddy: Fix caddy_certs.php style warning. A file should declare new symbols (classes, functions, constants, etc.) and cause no other side effects, or it should execute logic with side effects, but should not do both.
    
    * www/caddy: Ignore php style warnings for migration scripts, since the class names have to ignore PascalCase.
    
    * www/caddy: Correct style of php files so that no lines exceed 120 characters.
    
    * www/caddy: Refactor caddy_diagnostics.py and caddy_control.py for Python PEP 8. Fix: Shadows name 'action' from outer scope
    
    * www/caddy: Changelog add code cleanup.
    
    * www/caddy: Fix minor regression in refactored caddy_control.py script.
    
    * www/caddy: Fix indentation of Caddyfile template for better code readability. The template gets edited and read a lot so this fix really helps with maintainability.
    
    * www/caddy: Add changelog.
    
    * www/caddy: Fix caddy_certs.php style issue by removing the declared function and execute logic with side effects directly.
    
    * www/caddy: Re-add ValidationMessage to IntegerField because there are custom constraints that are not displayed by the default validation message.
    
    * www/caddy: Add error handling to caddy_control.py when action is empty. Make use of service_action and cmd_action clearer.
    
    * www/caddy: Remove unnecessary variable in caddy_diagnostics.py
    
    * www/caddy: Add validation message since there is a no IP constraint. Add same constraint to subdomains.
    
    * www/caddy: Re-add validation message to ToDomain field since multiple are allowed
    
    * www/caddy: Re-add validation message to clientIps field since multiple are allowed

--- www/caddy/src/opnsense/scripts/OPNsense/Caddy/caddy_certs.php
+++ www/caddy/src/opnsense/scripts/OPNsense/Caddy/caddy_certs.php
@@ -2,8 +2,7 @@
 <?php
 
 /*
- *    Copyright (C) 2023-2024 Cedrik Pischem
- *    Copyright (C) 2015 Deciso B.V.
+ *    Copyright (C) 2024 Cedrik Pischem
  *    All rights reserved.
  *
  *    Redistribution and use in source and binary forms, with or without
@@ -28,55 +27,50 @@
  *    POSSIBILITY OF SUCH DAMAGE.
  */
 
-require_once("config.inc");
+require_once "config.inc";
 
 use OPNsense\Core\Config;
 
 $configObj = Config::getInstance()->object();
 $temp_dir = '/var/db/caddy/data/caddy/certificates/temp/';
 
-function extract_and_save_certificates($configObj, $temp_dir)
-{
-    // Traverse through certificates
-    foreach ($configObj->cert as $cert) {
-        $cert_refid = (string)$cert->refid;
-        $cert_content = base64_decode((string)$cert->crt);
-        $key_content = base64_decode((string)$cert->prv);
-        $cert_chain = $cert_content;
+// Traverse through certificates
+foreach ($configObj->cert as $cert) {
+    $cert_refid = (string) $cert->refid;
+    $cert_content = base64_decode((string) $cert->crt);
+    $key_content = base64_decode((string) $cert->prv);
+    $cert_chain = $cert_content;
 
-        // Handle CA and possible intermediate CA to create a certificate bundle
-        if (!empty($cert->caref)) {
-            foreach ($configObj->ca as $ca) {
-                if ((string)$cert->caref == (string)$ca->refid) {
-                    $ca_content = base64_decode((string)$ca->crt);
-                    $cert_chain .= "\n" . $ca_content;
+    // Handle CA and possible intermediate CA to create a certificate bundle
+    if (!empty($cert->caref)) {
+        foreach ($configObj->ca as $ca) {
+            if ((string) $cert->caref === (string) $ca->refid) {
+                $ca_content = base64_decode((string) $ca->crt);
+                $cert_chain .= "\n" . $ca_content;
 
-                    if (!empty($ca->caref)) {
-                        foreach ($configObj->ca as $parent_ca) {
-                            if ((string)$ca->caref == (string)$parent_ca->refid) {
-                                $parent_ca_content = base64_decode((string)$parent_ca->crt);
-                                $cert_chain .= "\n" . $parent_ca_content;
-                                break;
-                            }
+                if (!empty($ca->caref)) {
+                    foreach ($configObj->ca as $parent_ca) {
+                        if ((string) $ca->caref === (string) $parent_ca->refid) {
+                            $parent_ca_content = base64_decode((string) $parent_ca->crt);
+                            $cert_chain .= "\n" . $parent_ca_content;
+                            break;
                         }
                     }
                 }
             }
         }
-
-        // Save the certificate chain and private key
-        file_put_contents($temp_dir . $cert_refid . '.pem', $cert_chain);
-        file_put_contents($temp_dir . $cert_refid . '.key', $key_content);
     }
 
-    // Traverse through CA certificates and save them
-    foreach ($configObj->ca as $ca) {
-        $ca_refid = (string)$ca->refid;
-        $ca_content = base64_decode((string)$ca->crt);
-
-        // Save the CA certificate
-        file_put_contents($temp_dir . $ca_refid . '.pem', $ca_content);
-    }
+    // Save the certificate chain and private key
+    file_put_contents($temp_dir . $cert_refid . '.pem', $cert_chain);
+    file_put_contents($temp_dir . $cert_refid . '.key', $key_content);
 }
 
-extract_and_save_certificates($configObj, $temp_dir);
+// Traverse through CA certificates and save them
+foreach ($configObj->ca as $ca) {
+    $ca_refid = (string) $ca->refid;
+    $ca_content = base64_decode((string) $ca->crt);
+
+    // Save the CA certificate
+    file_put_contents($temp_dir . $ca_refid . '.pem', $ca_content);
+}
