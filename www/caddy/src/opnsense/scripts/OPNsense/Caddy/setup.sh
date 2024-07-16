99628488ce0bf416ce69943e232bfd929e4583ac
Monviech <79600909+Monviech@users.noreply.github.com>
2024-07-16 13:28:22 +0200

    www/caddy: Add optional "DisableSuperuser" to run caddy as non root (#4081)
    * www/caddy: Add option to run caddy as www user instead of root superuser.
    
    * www/caddy: Add validation for DisableSuperuser that makes sure only ports 1024 or above are used.
    
    * www/caddy: Change rc.conf.d/caddy script to conditionally set the caddy_user and caddy_group to www when DisableSuperuser is activated.
    
    * www/caddy: Print the current Caddy user in the Caddyfile for easier support.
    
    * www/caddy: Improve setup.sh script. Create all directories, add root:www owners, differentiate between directories and files when creating permissions.
    
    * www/caddy: Remove copyright header in rc.conf.d/caddy.
    
    * www/caddy: Improve helptext for new DisableSuperuser feature in general.xml.
    
    * www/caddy: Fix style of rc.conf.d/caddy and caddy.php. Add changelog and bump plugin version.
    
    * www/caddy: Append additional message to DisableSuperuser that warns users about conflicting ports in otherwise hidden forms in the Reverse Proxy Domains view.
    
    * www/caddy: Add changelog.

--- www/caddy/src/opnsense/scripts/OPNsense/Caddy/setup.sh
+++ www/caddy/src/opnsense/scripts/OPNsense/Caddy/setup.sh
@@ -1,26 +1,71 @@
 #!/bin/sh
 
+#
+# Copyright (c) 2023-2024 Cedrik Pischem
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+# AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+# OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+
+# The directories are created as root:www with rwx permissions for both,
+# so the user can change in the GUI if caddy runs as root or www
+# If only ports 1024 and above are used, caddy can run as www user and group.
+
 # Define directories
-CADDY_DIR="/usr/local/etc/caddy"
-CADDY_CERTS_DIR="/var/db/caddy/data/caddy/certificates/temp"
-CADDY_LOG_DIR="/var/log/caddy/access"
-CADDY_CONF_DIR="${CADDY_DIR}/caddy.d"
-
-# Create custom directories with appropriate permissions
-mkdir -p "${CADDY_CERTS_DIR}"
-chown -R root:wheel "${CADDY_CERTS_DIR}"
-chmod -R 600 "${CADDY_CERTS_DIR}"
-
-mkdir -p "${CADDY_LOG_DIR}"
-chown -R root:wheel "${CADDY_LOG_DIR}"
-chmod -R 750 "${CADDY_LOG_DIR}"
+CADDY_CONF_DIR="/usr/local/etc/caddy"
+CADDY_DATA_DIR="/var/db/caddy"
+CADDY_LOG_DIR="/var/log/caddy"
+CADDY_RUN_DIR="/var/run/caddy"
+CADDY_CONF_CUSTOM_DIR="${CADDY_CONF_DIR}/caddy.d"
+CADDY_DATA_CUSTOM_DIR="${CADDY_DATA_DIR}/data/caddy/certificates/temp"
+CADDY_LOG_CUSTOM_DIR="${CADDY_LOG_DIR}/access"
 
 mkdir -p "${CADDY_CONF_DIR}"
-chown -R root:wheel "${CADDY_CONF_DIR}"
-chmod -R 750 "${CADDY_CONF_DIR}"
+mkdir -p "${CADDY_DATA_DIR}"
+mkdir -p "${CADDY_LOG_DIR}"
+mkdir -p "${CADDY_RUN_DIR}"
+mkdir -p "${CADDY_CONF_CUSTOM_DIR}"
+mkdir -p "${CADDY_DATA_CUSTOM_DIR}"
+mkdir -p "${CADDY_LOG_CUSTOM_DIR}"
 
-# Format and overwrite the Caddyfile
-(cd "${CADDY_DIR}" && /usr/local/bin/caddy fmt --overwrite)
+chown -R root:www "${CADDY_CONF_DIR}"
+chown -R root:www "${CADDY_DATA_DIR}"
+chown -R root:www "${CADDY_LOG_DIR}"
+chown -R root:www "${CADDY_RUN_DIR}"
 
-# Write custom certs from the OPNsense Trust Store into a directory where Caddy can read them
+# Directories need execute permissions to be accessible
+find "${CADDY_CONF_DIR}" -type d -exec chmod 770 {} +
+find "${CADDY_DATA_DIR}" -type d -exec chmod 770 {} +
+find "${CADDY_LOG_DIR}" -type d -exec chmod 770 {} +
+find "${CADDY_RUN_DIR}" -type d -exec chmod 770 {} +
+
+# Files can have read/write permissions
+find "${CADDY_CONF_DIR}" -type f -exec chmod 660 {} +
+find "${CADDY_DATA_DIR}" -type f -exec chmod 660 {} +
+find "${CADDY_LOG_DIR}" -type f -exec chmod 660 {} +
+find "${CADDY_RUN_DIR}" -type f -exec chmod 660 {} +
+
+# Format and overwrite the Caddyfile, this makes whitespace control in jinja2 unnecessary
+(cd "${CADDY_CONF_DIR}" && /usr/local/bin/caddy fmt --overwrite)
+
+# Write custom certs from the OPNsense Trust Store to CADDY_DATA_CUSTOM_DIR
 /usr/local/opnsense/scripts/OPNsense/Caddy/caddy_certs.php
