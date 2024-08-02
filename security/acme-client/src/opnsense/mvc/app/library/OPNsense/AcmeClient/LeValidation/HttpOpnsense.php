77d8538d85d264a917706f707bbf0a160792587d
Franco Fichtner <franco@opnsense.org>
2024-08-02 09:27:13 +0200

    security/acme-client: backend result changes slightly
    (cherry picked from commit 25ec135f0a20cecb94471860cf9bd27c0ee60588)

--- security/acme-client/src/opnsense/mvc/app/library/OPNsense/AcmeClient/LeValidation/HttpOpnsense.php
+++ security/acme-client/src/opnsense/mvc/app/library/OPNsense/AcmeClient/LeValidation/HttpOpnsense.php
@@ -74,8 +74,13 @@ class HttpOpnsense extends Base implements LeValidationInterface
         if (!empty((string)$this->config->http_opn_interface)) {
             $backend = new \OPNsense\Core\Backend();
             $response = json_decode($backend->configdpRun('interface address', [(string)$this->config->http_opn_interface]));
-            if (!empty($response->address)) {
-                $iplist[] = $response->address;
+            // XXX Returns both IPv4 and IPv6 now. While "[0]" and
+            // "[1]" should remain in this order it would make sense
+            // to ensure "family" matches "inet" or "inet6" and/or
+            // pull both addresses for missing IPv6 support depending
+            // on how this should work.
+            if (!empty($response[0]->address)) {
+                $iplist[] = $response[0]->address;
             }
         }
 
