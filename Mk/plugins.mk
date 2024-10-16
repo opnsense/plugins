a85edbaea9ec4169d47769e1d210f66d8d71f2c5
Franco Fichtner <franco@opnsense.org>
2024-10-16 08:48:51 +0200

    plugins: add 'commit' target to ease a bit of typing
    (cherry picked from commit 1a64cab8cb8f5b1f8f1a5d83de25652940e9ae39)

--- Mk/plugins.mk
+++ Mk/plugins.mk
@@ -278,6 +278,9 @@ WRKDIR?=${.CURDIR}/work
 WRKSRC?=${WRKDIR}/src
 PKGDIR?=${WRKDIR}/pkg
 
+ensure-workdirs:
+	@mkdir -p ${WRKSRC} ${PKGDIR}
+
 package: check
 	@rm -rf ${WRKSRC}
 	@mkdir -p ${WRKSRC} ${PKGDIR}
@@ -452,5 +455,9 @@ test: check
 		    phpunit --configuration PHPunit.xml \
 		    ${.CURDIR}/src/opnsense/mvc/tests; \
 	fi
+
+commit: ensure-workdirs
+	@echo -n "${.CURDIR:C/\// /g:[-2]}/${.CURDIR:C/\// /g:[-1]}: " > \
+	    ${WRKDIR}/.commitmsg && git commit -eF ${WRKDIR}/.commitmsg .
 
 .PHONY:	check plist-fix
