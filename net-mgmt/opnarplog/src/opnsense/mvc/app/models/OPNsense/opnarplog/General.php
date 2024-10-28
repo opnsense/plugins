a3ae412fd54b22afbc085474a309d605a87d8b29
Manuel <mr-manuel@outlook.it>
2024-10-28 14:41:37 +0100

    Add OPNarplog

---
+++ net-mgmt/opnarplog/src/opnsense/mvc/app/models/OPNsense/opnarplog/General.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * Copyright (C) 2025 github.com/mr-manuel
+ * All rights reserved.
+ */
+
+namespace OPNsense\Opnarplog;
+
+use OPNsense\Base\BaseModel;
+
+class General extends BaseModel
+{
+}
