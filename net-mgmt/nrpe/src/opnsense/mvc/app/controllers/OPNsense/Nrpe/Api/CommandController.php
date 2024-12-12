06a28c3496eabdffc1216a8630b573a4f6c2df2b
Franco Fichtner <franco@opnsense.org>
2024-12-12 08:46:01 +0100

    net-mgmt/nrpe: remove sessionClose()

--- net-mgmt/nrpe/src/opnsense/mvc/app/controllers/OPNsense/Nrpe/Api/CommandController.php
+++ net-mgmt/nrpe/src/opnsense/mvc/app/controllers/OPNsense/Nrpe/Api/CommandController.php
@@ -1,31 +1,29 @@
 <?php
 
-/**
- *    Copyright (C) 2019 Michael Muenz <m.muenz@gmail.com>
+/*
+ * Copyright (C) 2019 Michael Muenz <m.muenz@gmail.com>
+ * All rights reserved.
  *
- *    All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- *    Redistribution and use in source and binary forms, with or without
- *    modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
  *
- *    1. Redistributions of source code must retain the above copyright notice,
- *       this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
  *
- *    2. Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *
- *    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
- *    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
- *    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
- *    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- *    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- *    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- *    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- *    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *    POSSIBILITY OF SUCH DAMAGE.
- *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 namespace OPNsense\Nrpe\Api;
@@ -39,25 +37,29 @@ class CommandController extends ApiMutableModelControllerBase
 
     public function searchCommandAction()
     {
-        return $this->searchBase('commands.command', array("enabled", "name", "nrpecommand", "arguments"));
+        return $this->searchBase('commands.command', ['enabled', 'name', 'nrpecommand', 'arguments']);
     }
+
     public function getCommandAction($uuid = null)
     {
-        $this->sessionClose();
         return $this->getBase('command', 'commands.command', $uuid);
     }
+
     public function addCommandAction()
     {
         return $this->addBase('command', 'commands.command');
     }
+
     public function delCommandAction($uuid)
     {
         return $this->delBase('commands.command', $uuid);
     }
+
     public function setCommandAction($uuid)
     {
         return $this->setBase('command', 'commands.command', $uuid);
     }
+
     public function toggleCommandAction($uuid)
     {
         return $this->toggleBase('commands.command', $uuid);
