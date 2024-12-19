80c2623bd581f4586b09eb54ae30b2e0965cf60c
Franco Fichtner <franco@opnsense.org>
2024-12-19 19:06:56 +0100

    security/tailscale: minor changes to previous

--- security/tailscale/src/opnsense/www/js/widgets/Tailscale.js
+++ security/tailscale/src/opnsense/www/js/widgets/Tailscale.js
@@ -24,7 +24,7 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-export default class Wireguard extends BaseTableWidget {
+export default class Tailscale extends BaseTableWidget {
     constructor() {
         super();
     }
