53bcf914aa68fd9acf22525fb0746bdfa2731e02
Ben Smithurst <github@bensmithurst.com>
2025-01-01 18:13:06 +0000

    exit node support

---
+++ security/tailscale/src/opnsense/mvc/app/models/OPNsense/Tailscale/FieldTypes/ExitNodeField.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace OPNsense\Tailscale\FieldTypes;
+
+use OPNsense\Base\FieldTypes\BaseListField;
+use OPNsense\Core\Backend;
+
+class ExitNodeField extends BaseListField
+{
+    private static array $internalCacheOptionList = [];
+    protected $internalIsContainer = false;
+
+    protected function actionPostLoadingEvent()
+    {
+        if (empty(self::$internalCacheOptionList)) {
+            $response = json_decode(trim((new Backend())->configdRun('tailscale tailscale-status')), true);
+            $exitNodes = [];
+            foreach ($response['Peer'] as $peer) {
+                if ($peer['ExitNodeOption']) {
+                    $exitNodes[$peer['HostName']] = $peer['HostName'];
+                }
+            }
+            $exitNodes['foo'] = 'bar';
+
+            self::$internalCacheOptionList = $exitNodes;
+        }
+        $this->internalOptionList = self::$internalCacheOptionList;
+    }
+}
+
+# vim:ts=4 sw=4 et:
