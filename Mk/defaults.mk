64d42dc335affc9aac54b5f54e60aeeac225e767
Franco Fichtner <franco@opnsense.org>
2025-01-24 09:59:18 +0100

    plugins: add reset target
    (cherry picked from commit 7a7a3138a3170fc9a70d95ee9ae6f383eb2a0644)

--- Mk/defaults.mk
+++ Mk/defaults.mk
@@ -1,4 +1,4 @@
-# Copyright (c) 2016-2024 Franco Fichtner <franco@opnsense.org>
+# Copyright (c) 2016-2025 Franco Fichtner <franco@opnsense.org>
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -160,4 +160,9 @@ log:
 push:
 	@git checkout ${PLUGIN_STABLE}
 	@git push
+	@git checkout ${PLUGIN_MAIN}
+
+reset:
+	@git checkout ${PLUGIN_STABLE}
+	@git reset --hard HEAD~1
 	@git checkout ${PLUGIN_MAIN}
