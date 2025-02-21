ad5c32c4b00812ea09343c96cc5e2c14bac2c3c6
Ethazeriel <ethazeriel@gmail.com>
2025-02-21 14:49:12 +1300

    sysutils/dmidecode: style pass, create ACL

--- sysutils/dmidecode/src/opnsense/mvc/app/controllers/OPNsense/DmiDecode/Api/ServiceController.php
+++ sysutils/dmidecode/src/opnsense/mvc/app/controllers/OPNsense/DmiDecode/Api/ServiceController.php
@@ -1,20 +1,22 @@
 <?php
+
 namespace OPNsense\dmidecode\Api;
 
-use \OPNsense\Base\ApiControllerBase;
-use \OPNsense\Core\Backend;
+use OPNsense\Base\ApiControllerBase;
+use OPNsense\Core\Backend;
+
 class ServiceController extends ApiControllerBase
 {
-  public function getAction()
-  {
-    $status = "failed";
-    $system = array();
-    $bios = array();
-    if ($this->request->isGet()) {
-      $system = parse_ini_string(trim((new Backend())->configdRun('dmidecode system')), FALSE, INI_SCANNER_RAW);
-      $bios = parse_ini_string(trim((new Backend())->configdRun('dmidecode bios')), FALSE, INI_SCANNER_RAW);
-      $status = "ok";
+    public function getAction()
+    {
+        $status = "failed";
+        $system = array();
+        $bios = array();
+        if ($this->request->isGet()) {
+            $system = parse_ini_string(trim((new Backend())->configdRun('dmidecode system')), false, INI_SCANNER_RAW);
+            $bios = parse_ini_string(trim((new Backend())->configdRun('dmidecode bios')), false, INI_SCANNER_RAW);
+            $status = "ok";
+        }
+        return ["status" => $status, "system" => $system, "bios" => $bios];
     }
-    return ["status" => $status, "system" => $system, "bios" => $bios];
-  }
 }
