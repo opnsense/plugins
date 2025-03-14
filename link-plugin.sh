3391414d88b4e34ac3b49df0dd2d8054c160272d
Freddie Sackur <freddie.git@dustyfox.uk>
2025-03-14 16:22:59 +0000

    polish symlink script

--- link-plugin.sh
+++ link-plugin.sh
@@ -1,37 +1,68 @@
 #! /usr/bin/env bash
 
-if [[ ! -d "/app" ]] ; then
-    ln -s /usr/local/opnsense/mvc/app
+helpmsg="Usage: $(realpath $0) [-r|--root-dir ROOTDIR] PLUGIN [...PLUGIN]"
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -h|--help|help)
+      echo "$helpmsg"
+      exit 0
+      ;;
+    -r|--root-dir)
+      ROOTDIR="$2"
+      shift
+      shift
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1")
+      shift
+      ;;
+  esac
+done
+
+if [[ -z "$POSITIONAL_ARGS" ]]; then
+    echo $helpmsg 1>&2
+    exit 1
 fi
 
-for folder in "controllers" "models" "views"; do
-    if [[ ! -d "/app/$folder/OPNsense/OpenApi" ]] ; then
-        pushd "/app/$folder/OPNsense"
-        ln -s "/gitroot/opnsense/plugins/devel/openapi/src/opnsense/mvc/app/$folder/OPNsense/OpenApi"
+ROOTDIR="${ROOTDIR:-/usr/}"
+
+if [[ ! -d "/app" ]] ; then
+    pushd /
+    ln -s /usr/local/opnsense/mvc/app
+    popd
+fi
+
+for plugin in ${POSITIONAL_ARGS[@]}; do
+    plugin_lower=$(echo $plugin | tr '[:upper:]' '[:lower:]')
+
+    for folder in "controllers" "models" "views"; do
+        if [[ ! -d "/app/$folder/OPNsense/$plugin" ]] ; then
+            pushd "/app/$folder/OPNsense"
+            ln -s "$ROOTDIR/plugins/devel/$plugin_lower/src/opnsense/mvc/app/$folder/OPNsense/$plugin"
+            popd
+        fi
+    done
+
+    if [[ ! -d "/usr/local/opnsense/service/templates/OPNsense/$plugin" ]] ; then
+        pushd /usr/local/opnsense/service/templates/OPNsense/
+        ln -s "$ROOTDIR/plugins/devel/$plugin_lower/src/opnsense/service/templates/OPNsense/$plugin"
+        popd
+    fi
+
+    if [[ ! -f "/usr/local/opnsense/service/conf/actions.d/actions_$plugin_lower.conf" ]] ; then
+        pushd /usr/local/opnsense/service/conf/actions.d/
+        ln -s "$ROOTDIR/plugins/devel/$plugin_lower/src/opnsense/service/conf/actions.d/actions_$plugin_lower.conf"
+        popd
+    fi
+
+    if [[ ! -f "/usr/local/opnsense/scripts/OPNsense/$plugin" ]] ; then
+        pushd /usr/local/opnsense/scripts/OPNsense
+        ln -s "$ROOTDIR/plugins/devel/$plugin_lower/src/opnsense/scripts/" $plugin
         popd
     fi
 done
-
-if [[ ! -d "/usr/local/opnsense/service/templates/OPNsense/OpenApi" ]] ; then
-    pushd /usr/local/opnsense/service/templates/OPNsense/
-    ln -s "/gitroot/opnsense/plugins/devel/openapi/src/opnsense/service/templates/OPNsense/OpenApi"
-    popd
-fi
-
-if [[ ! -f "/usr/local/opnsense/service/conf/actions.d/actions_openapi.conf" ]] ; then
-    pushd /usr/local/opnsense/service/conf/actions.d/
-    ln -s "/gitroot/opnsense/plugins/devel/openapi/src/opnsense/service/conf/actions.d/actions_openapi.conf"
-    popd
-fi
-
-if [[ ! -f "/usr/local/opnsense/scripts/OPNsense/OpenApi" ]] ; then
-    pushd /usr/local/opnsense/scripts/OPNsense
-    ln -s "/gitroot/opnsense/plugins/devel/openapi/src/opnsense/scripts/" OpenApi
-    popd
-fi
-
-# if [[ ! -d "/usr/local/etc/openapi" ]] ; then
-#     pushd /usr/local/etc/
-#     ln -s "/gitroot/opnsense/plugins/devel/openapi/src/opnsense/mvc/app/$folder/OPNsense/OpenApi"
-#     popd
-# fi
