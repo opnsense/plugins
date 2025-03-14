ad9b30ef73f0188cf128c3bdf1300521b09676ed
Freddie Sackur <freddie.git@dustyfox.uk>
2025-03-14 21:03:12 +0000

    tidy python script

--- devel/openapi/src/opnsense/scripts/generate_openapi_spec.py
+++ devel/openapi/src/opnsense/scripts/generate_openapi_spec.py
@@ -1,152 +1,53 @@
 #! /usr/bin/env python3
 
+import sys
+from typing import List, Dict, TypedDict, Literal
+
 # https://github.com/globality-corp/openapi
 from openapi.model import Swagger, Info, Operation, PathItem, Paths, Response, Responses
 
-sample_data = {
-    'opnsense': [
-        [
-            {
-                'method': 'GET',
-                'module': 'opnsense',
-                'controller': 'index',
-                'is_abstract': False,
-                'base_class': 'IndexController',
-                'command': 'index',
-                'parameters': '',
-                'filename': 'IndexController.php',
-                'model_filename': None,
-                'type': 'Resources'
-            }
-        ]
-    ],
-    'openapi': [
-        [
-            {
-                'method': 'GET',
-                'module': 'openapi',
-                'controller': 'service',
-                'is_abstract': False,
-                'base_class': 'ApiMutableServiceControllerBase',
-                'command': 'reconfigure',
-                'parameters': '',
-                'filename': 'ServiceController.php',
-                'model_filename': '/usr/plugins/devel/openapi/src/opnsense/mvc/app/models/OPNsense/OpenApi/OpenApi.xml',
-                'type': 'Service'
-            },
-            {
-                'type': 'Service',
-                'method': 'POST',
-                'module': 'openapi',
-                'controller': 'service',
-                'is_abstract': False,
-                'base_class': 'ApiMutableServiceControllerBase',
-                'command': 'reconfigure',
-                'parameters': '',
-                'filename': 'ServiceController.php',
-                'model_filename': '/usr/plugins/devel/openapi/src/opnsense/mvc/app/models/OPNsense/OpenApi/OpenApi.xml'
-            },
-            {
-                'type': 'Service',
-                'method': 'POST',
-                'module': 'openapi',
-                'controller': 'service',
-                'is_abstract': False,
-                'base_class': 'ApiMutableServiceControllerBase',
-                'command': 'restart',
-                'parameters': '',
-                'filename': 'ServiceController.php',
-                'model_filename': '/usr/plugins/devel/openapi/src/opnsense/mvc/app/models/OPNsense/OpenApi/OpenApi.xml'
-            },
-            {
-                'type': 'Service',
-                'method': 'POST',
-                'module': 'openapi',
-                'controller': 'service',
-                'is_abstract': False,
-                'base_class': 'ApiMutableServiceControllerBase',
-                'command': 'start',
-                'parameters': '',
-                'filename': 'ServiceController.php',
-                'model_filename': '/usr/plugins/devel/openapi/src/opnsense/mvc/app/models/OPNsense/OpenApi/OpenApi.xml'
-            },
-            {
-                'type': 'Service',
-                'method': 'GET',
-                'module': 'openapi',
-                'controller': 'service',
-                'is_abstract': False,
-                'base_class': 'ApiMutableServiceControllerBase',
-                'command': 'status',
-                'parameters': '',
-                'filename': 'ServiceController.php',
-                'model_filename': '/usr/plugins/devel/openapi/src/opnsense/mvc/app/models/OPNsense/OpenApi/OpenApi.xml'
-            },
-            {
-                'type': 'Service',
-                'method': 'POST',
-                'module': 'openapi',
-                'controller': 'service',
-                'is_abstract': False,
-                'base_class': 'ApiMutableServiceControllerBase',
-                'command': 'stop',
-                'parameters': '',
-                'filename': 'ServiceController.php',
-                'model_filename': '/usr/plugins/devel/openapi/src/opnsense/mvc/app/models/OPNsense/OpenApi/OpenApi.xml'
-            }
-        ],
-        [
-            {
-                'type': 'Service',
-                'method': 'GET',
-                'module': 'openapi',
-                'controller': 'settings',
-                'is_abstract': False,
-                'base_class': 'ApiMutableModelControllerBase',
-                'command': 'get',
-                'parameters': '',
-                'filename': 'SettingsController.php',
-                'model_filename': '/usr/plugins/devel/openapi/src/opnsense/mvc/app/models/OPNsense/OpenApi/OpenApi.xml'
-            },
-            {
-                'type': 'Service',
-                'method': 'POST',
-                'module': 'openapi',
-                'controller': 'settings',
-                'is_abstract': False,
-                'base_class': 'ApiMutableModelControllerBase',
-                'command': 'set',
-                'parameters': '',
-                'filename': 'SettingsController.php',
-                'model_filename': '/usr/plugins/devel/openapi/src/opnsense/mvc/app/models/OPNsense/OpenApi/OpenApi.xml'
-            }
-        ]
-    ]
-}
-
-
 from collect_api_endpoints import collect_api_modules
-collected_modules = sample_data
-# collected_modules = collect_api_modules("/usr/plugins")
 
-models = {}
-paths = {}
-for list_of_lists in collected_modules.values():
-    modules = [m for sub_list in list_of_lists for m in sub_list]
-    for module in modules:
-        if module.get('module') != "openapi":
-            continue
+class Endpoint(TypedDict):
+    method: Literal["GET"] | Literal["POST"] | Literal["*"]
+    module: str
+    controller: str
+    command: str
+    parameters: str
+    is_abstract: bool
+    base_class: str
+    filename: str
+    model_filename: str | None
+    type: str
 
-        path = f"/{module.get('module')}/{module.get('controller')}/{module.get('command')}"
 
-        model_filename = module.get('model_filename')
+def get_endpoints(path: str) -> List[Endpoint]:
+    collected_endpoints = collect_api_modules(path)
+    endpoints = []
+    for list_of_lists in collected_endpoints.values():
+        _endpoints = [m for sub_list in list_of_lists for m in sub_list]
+        endpoints.extend(_endpoints)
+    return endpoints
+
+
+def get_spec(endpoints: List[Endpoint]):
+    models = {}
+    paths = {}
+    for endpoint in endpoints:
+        path = f'/{endpoint["module"]}/{endpoint["controller"]}/{endpoint["command"]}'
+
+        model_filename = endpoint["model_filename"]
+        # TODO: parse xml, generate json schema
+
+        # parameters = endpoint["parameters"]
+        parameters = []  # TODO
 
         items = {}
-        method = module.get('method', 'GET').lower()
+        method = endpoint.get("method", "GET").lower()
         methods = ["get", "post"] if method == "*" else [method]
         for method in methods:
             items[method] = Operation(
-                parameters=[],
+                parameters=parameters,
                 responses=Responses({
                     "200": Response(
                         description="TODO - parse description",
@@ -156,18 +57,33 @@ for list_of_lists in collected_modules.values():
 
         paths[path] = PathItem(**items)
 
+    spec = Swagger(
+        swagger="2.0",
+        info=Info(
+            title="OPNsense",
+            version="1.0.0",
+        ),
+        basePath="/api",
+        paths = Paths(paths)
+    )
 
-swagger = Swagger(
-    swagger="2.0",
-    info=Info(
-        title="OPNsense",
-        version="1.0.0",
-    ),
-    basePath="/api",
-    paths = Paths(paths)
-)
+    spec.validate()
 
-swagger.validate()
+    return spec
 
-with open("/usr/local/opnsense/www/openapi.json", mode="w") as file:
-    file.write(swagger.dumps())
+
+def write_spec(spec: Swagger, path: str):
+    with open(path, mode="w") as file:
+        file.write(spec.dumps())
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} OUTPUT_FILE")
+        exit(1)
+
+    source_path = "/usr/plugins"
+    output_file = sys.argv[1]
+    endpoints = get_endpoints(source_path)
+    spec = get_spec(endpoints)
+    write_spec(spec, output_file)
