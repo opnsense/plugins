be70ca0a3c8f02a139d8881020206670e5744a77
marco <marco@crowdsec.net>
2025-05-13 16:12:34 +0200

    some python typing

--- security/crowdsec/src/opnsense/scripts/OPNsense/CrowdSec/reconfigure.py
+++ security/crowdsec/src/opnsense/scripts/OPNsense/CrowdSec/reconfigure.py
@@ -3,32 +3,33 @@
 import logging
 import json
 import urllib.parse
+from typing import cast, Any
 import yaml
 
 logging.basicConfig(level=logging.INFO)
 
 
-def load_config(filename):
+def load_config(filename: str) -> dict[str, Any]:
     with open(filename) as fin:
         return yaml.safe_load(fin)
 
 
 # only save if some value has changed
-def save_config(filename, new_config):
+def save_config(filename: str, new_config: dict[str, Any]):
     old_config = load_config(filename)
     if old_config != new_config:
         with open(filename, 'w') as fout:
             yaml.dump(new_config, fout)
 
 
-def get_netloc(settings):
+def get_netloc(settings: dict[str, str]):
     # defaults if config has not been saved yet
     listen_address = settings.get('lapi_listen_address', '127.0.0.1')
     listen_port = settings.get('lapi_listen_port', '8080')
     return '{}:{}'.format(listen_address, listen_port)
 
 
-def get_new_url(old_url, settings):
+def get_new_url(old_url: str, settings: dict[str, str]):
     old_tuple = urllib.parse.urlsplit(old_url)
     new_tuple = old_tuple._replace(netloc=get_netloc(settings))
     new_url = urllib.parse.urlunsplit(new_tuple)
@@ -39,7 +40,7 @@ def get_new_url(old_url, settings):
     return new_url
 
 
-def configure_agent(settings):
+def configure_agent(settings: dict[str, str]):
     config_path = '/usr/local/etc/crowdsec/config.yaml'
     config = load_config(config_path)
 
@@ -53,7 +54,7 @@ def configure_agent(settings):
     save_config(config_path, config)
 
 
-def configure_lapi(settings):
+def configure_lapi(settings: dict[str, str]):
     config_path = '/usr/local/etc/crowdsec/config.yaml'
     config = load_config(config_path)
 
@@ -63,7 +64,7 @@ def configure_lapi(settings):
     save_config(config_path, config)
 
 
-def configure_lapi_credentials(settings):
+def configure_lapi_credentials(settings: dict[str, str]):
     config_path = '/usr/local/etc/crowdsec/local_api_credentials.yaml'
     config = load_config(config_path)
 
@@ -73,7 +74,7 @@ def configure_lapi_credentials(settings):
     save_config(config_path, config)
 
 
-def configure_bouncer(settings):
+def configure_bouncer(settings: dict[str, str]):
     config_path = '/usr/local/etc/crowdsec/bouncers/crowdsec-firewall-bouncer.yaml'
     config = load_config(config_path)
 
@@ -92,7 +93,7 @@ def configure_bouncer(settings):
 def main():
     try:
         with open('/usr/local/etc/crowdsec/opnsense/settings.json') as f:
-            settings = json.load(f)
+            settings = cast(dict[str, str], json.load(f))
     except FileNotFoundError:
         logging.info("settings.json not found, won't change crowdsec config")
         return
