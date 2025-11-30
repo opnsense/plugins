c60d429e461032ad42b2b0c888a937c61b2f1b37
Anton Avramov <lukav@lukav.com>
2025-11-30 17:04:29 -0500

    Update requested changes

--- security/acme-client/src/opnsense/mvc/app/library/OPNsense/AcmeClient/LeValidation/DnsZoneedit.php
+++ security/acme-client/src/opnsense/mvc/app/library/OPNsense/AcmeClient/LeValidation/DnsZoneedit.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (C) 2020 Frank Wall
+ * Copyright (C) 2025 Anton Avramov
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
