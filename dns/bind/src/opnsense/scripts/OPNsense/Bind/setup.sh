8f4daa869f01ef203efa73ce4926f0945a0f1b11
Diego Rivera <diego.rivera@armedia.com>
2025-12-24 11:01:53 -0600

    Add some scripts to automatically handle JNL file pollution

--- dns/bind/src/opnsense/scripts/OPNsense/Bind/setup.sh
+++ dns/bind/src/opnsense/scripts/OPNsense/Bind/setup.sh
@@ -5,3 +5,13 @@ for DIR in /var/run/named /var/dump /var/stats /var/log/named /usr/local/etc/nam
 	chown -R bind:bind ${DIR}
 	chmod 755 ${DIR}
 done
+
+# This should help clean out orphaned journal files
+if ! rndc sync -clean ; then
+	# If the RNDC command didn't work, we should probably clean
+	# the files out manually because on a clean shutdown they
+	# would be cleared out by "service named stop" ... so if
+	# they're still around it means something went down HARD and
+	# thus the files are suspect and could derail BIND9 startup
+	find /usr/local/etc/namedb/primary -type f -name '*.jnl' -print -delete
+fi
