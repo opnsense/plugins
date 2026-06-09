b80fb3a1705e67abf556c819cff7b11152e8b0c3
Richard Aspden <rick+github@insanityinside.net>
2026-06-09 15:51:35 +0100

    Cleanup for actions and rc.conf.d template

--- net/cloudflared/src/opnsense/service/templates/OPNsense/Cloudflared/rc.conf.d
+++ net/cloudflared/src/opnsense/service/templates/OPNsense/Cloudflared/rc.conf.d
@@ -1,10 +1,10 @@
 {% if helpers.exists('OPNsense.Cloudflared.general.enabled') and OPNsense.Cloudflared.general.enabled == '1' %}
 cloudflared_enable="YES"
-{% else %}
-cloudflared_enable="NO"
-{% endif %}
 cloudflared_conf="/usr/local/etc/cloudflared/config.yml"
 {% if helpers.exists('OPNsense.Cloudflared.general.token') %}
 cloudflared_env="TUNNEL_TOKEN={{ OPNsense.Cloudflared.general.token|trim }}"
 {% endif %}
 cloudflared_mode_options="run"
+{% else %}
+cloudflared_enable="NO"
+{% endif %}
