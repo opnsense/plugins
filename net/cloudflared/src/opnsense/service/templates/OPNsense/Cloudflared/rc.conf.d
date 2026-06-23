6a7015bc816e3228ddbcd326e943fec91e6c2737
Richard Aspden <rick+github@insanityinside.net>
2026-06-23 16:54:00 +0100

    Remove now unnecessary 'auto' check in rc.conf.d

--- net/cloudflared/src/opnsense/service/templates/OPNsense/Cloudflared/rc.conf.d
+++ net/cloudflared/src/opnsense/service/templates/OPNsense/Cloudflared/rc.conf.d
@@ -3,7 +3,7 @@ cloudflared_enable="YES"
 cloudflared_conf="/usr/local/etc/cloudflared/config.yml"
 {% if helpers.exists('OPNsense.Cloudflared.general.token') %}
 {% set ns = namespace(env='TUNNEL_TOKEN=' + OPNsense.Cloudflared.general.token|trim) %}
-{% if helpers.exists('OPNsense.Cloudflared.general.edge_ip_version') and OPNsense.Cloudflared.general.edge_ip_version != 'auto' %}
+{% if helpers.exists('OPNsense.Cloudflared.general.edge_ip_version') %}
 {% set ns.env = ns.env + ' TUNNEL_EDGE_IP_VERSION=' + OPNsense.Cloudflared.general.edge_ip_version %}
 {% endif %}
 cloudflared_env="{{ ns.env }}"
