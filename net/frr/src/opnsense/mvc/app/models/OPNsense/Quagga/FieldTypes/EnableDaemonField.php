7900a323ca3e66ce495fdba49c6fe67eb876d3ae
Franco Fichtner <franco@opnsense.org>
2026-08-12 10:24:01 +0200

    net/frr: sync with master

--- net/frr/src/opnsense/mvc/app/models/OPNsense/Quagga/FieldTypes/EnableDaemonField.php
+++ net/frr/src/opnsense/mvc/app/models/OPNsense/Quagga/FieldTypes/EnableDaemonField.php
@@ -2,7 +2,6 @@
 
 /*
  * Copyright (C) 2026 Deciso B.V.
- * Copyright (C) 2017 Fabian Franz
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -12,40 +11,42 @@
  *    this list of conditions and the following disclaimer.
  *
  * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions in the documentation and/or other
- *    materials provided with the distribution.
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
  *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
  * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
  * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
  * AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
- * OR CONSEQUENTIAL DAMAGES INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
  * SUBSTITUTE GOODS OR SERVICES, LOSS OF USE, DATA, OR PROFITS, OR BUSINESS
- * INTERRUPTION HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
  * CONTRACT, STRICT LIABILITY, OR TORT INCLUDING NEGLIGENCE OR OTHERWISE
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-namespace OPNsense\Quagga;
+namespace OPNsense\Quagga\FieldTypes;
 
-use OPNsense\Base\BaseModel;
+use OPNsense\Base\FieldTypes\OptionField;
 use OPNsense\Core\Config;
-use OPNsense\Quagga\FieldTypes\EnableDaemonField;
 
-class General extends BaseModel
+class EnableDaemonField extends OptionField
 {
-    public function serializeToConfig($validateFullModel = false, $disable_validation = false)
+    public const CONFIG_SECTIONS = ['bfd', 'bgp', 'ospf', 'ospf6', 'rip', 'static'];
+    protected function actionPostLoadingEvent()
     {
-        $result = parent::serializeToConfig($validateFullModel, $disable_validation);
-        $config = Config::getInstance()->object();
-        $enabledSections = $this->daemons->getValues();
+        $quagga = Config::getInstance()->object()->OPNsense->quagga ?? null;
+        $enabled = [];
 
-        foreach (EnableDaemonField::CONFIG_SECTIONS as $section) {
-            $config->OPNsense->quagga->{$section}->enabled =
-               in_array($section, $enabledSections, true) ? '1' : '0';
+        foreach (self::CONFIG_SECTIONS as $section) {
+            if (!empty($quagga?->{$section}?->enabled)) {
+                $enabled[] = $section;
+            }
         }
 
-        return $result;
+        $this->setValue(implode(',', $enabled));
+
+        return parent::actionPostLoadingEvent();
     }
 }
